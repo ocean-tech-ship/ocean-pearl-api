8b6fe803c5d235774c5f322fa6a9e97de71ba748
Dominik Nolte <dominik.nolte@gemedo.de>
2021-04-15 00:46:29 +0200

    update(database): add new conditions to the company schema, add token option as salary

--- src/database/model/job.model.ts
+++ src/database/model/job.model.ts
@@ -1,12 +1,17 @@
 import { model, Schema, Document } from 'mongoose';
 import { CompanyInterface } from './company.model';
+import { CountryEnum } from '../enums/country.enum';
+import { TokenOptionEnum } from '../enums/token-option.enum';
 
 import mongoosePaginate = require('mongoose-paginate-v2');
 
 export interface JobInterface extends Document {
     title: string,
     description: string,
-    location: string,
+    location: CountryEnum,
+    tokenSalaryOption?: TokenOptionEnum,
+    token?: string,
+    tokenAmount: number,
     salaryFrom: number,
     salaryTo: number,
     startDate: Date,
@@ -17,23 +22,48 @@ const jobSchema: Schema = new Schema({
     title: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        maxLength: 256
     },
     description: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        maxLength: 2048
     },
     location: {
         type: String,
         require: true,
-        trim: true
+        trim: true,
+        maxLength: 4,
+        uppercase: true
+    },
+    tokenSalaryOption: {
+        type: String,
+        maxLength: 4,
+        required: false,
+        default: 'none'
+    },
+    token: {
+        type: String,
+        maxLength: 32,
+        required: false,
+        default: 'none'
+    },
+    tokenAmount: {
+        type: Number,
+        required: false,
+        max: 999999
     },
     salaryFrom: {
-        type: Number
+        type: Number,
+        min: 0,
+        max: 999999
     },
     salaryTo: {
-        type: Number
+        type: Number,
+        min: 0,
+        max: 999999
     },
     startDate: {
         type: Date,
