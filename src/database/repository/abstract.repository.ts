17eb8c41efae1532ed2529e365b8359f5da540b8
Dominik Nolte <dominik.nolte@gemedo.de>
2021-04-15 00:51:17 +0200

    refactor(database): use try-catch instead of then-catch, cast mongoose responses to the given generic type

--- src/database/repository/abstract.repository.ts
+++ src/database/repository/abstract.repository.ts
@@ -1,8 +1,10 @@
 import { Document, FilterQuery, Model } from "mongoose";
-import { RepositoryInterface } from "./repository.inteface";
+import { MongooseDeleteResponseInterface } from "../interfaces/mongoose-delete-response.interface";
+import { RepositoryInterface } from "../interfaces/repository.inteface";
 
-export abstract class AbstractRepository<T extends Document> implements RepositoryInterface<T> {
-
+export abstract class AbstractRepository<T extends Document>
+    implements RepositoryInterface<T> {
+        
     private model: Model<Document>;
 
     constructor(model: Model<Document>) {
@@ -10,69 +12,58 @@ export abstract class AbstractRepository<T extends Document> implements Reposito
     }
 
     public async getByID(id: string): Promise<T> {
-        let model: T;
 
-        await this.model.findById(id)
-        .then((response: T) => {
-            model = response;
-        }).catch((error: Error) => {
+        try {
+            return await this.model.findById(id) as T;
+        } catch (error: any) {
+
             throw error;
-        });
-    
-        return model;
+        }
     }
 
     public async getAll(query?: FilterQuery<any>): Promise<T[]> {
-        let entries: T[];
 
-        await this.model.find(query || {})
-        .then((response: T[]) => {
-            entries = response;
-        }).catch((error: Error) => {
+        try {
+            return await this.model.find(query || {}) as T[];
+        } catch (error: any) {
+
             throw error;
-        });
-
-        return entries;
+        }
     }
 
     public async update(model: T): Promise<boolean> {
-        let response: boolean = false;
 
-        await this.model.findOneAndUpdate(
-            {_id: model._id},
-            model
-        ).then(() => {
-            response = true;
-        }).catch((error: Error) => {
+        try {
+            const response: T = await this.model.findOneAndUpdate({ _id: model._id }, model) as T;
+
+            return response !== null;
+        } catch (error: any) {
+
             throw error;
-        });
-
-        return response;
+        }
     }
 
     public async create(model: T): Promise<boolean> {
-        let response: boolean = false;
 
-        await this.model.create(model)
-        .then(() => {
-            response = true;
-        }).catch((error: Error) => {
+        try {
+            const response: T = await this.model.create(model) as T;
+
+            return response !== null;
+        } catch (error: any) {
+
             throw error;
-        });
-
-        return response;
+        }
     }
 
     public async delete(id: string): Promise<boolean> {
-        let response: boolean = false;
-        
-        await this.model.deleteOne({'_id': id})
-        .then(() => {
-            response = true;
-        }).catch((error: Error) => {
-            throw error
-        });
 
-        return response;
+        try {
+            const response: MongooseDeleteResponseInterface = await this.model.deleteOne({ _id: id });
+
+            return response.deletedCount === 1;
+        } catch (error: any) {
+
+            throw error;
+        }
     }
 }
