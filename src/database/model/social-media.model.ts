5d05cb354d6eb327506b095d2080b9dafd4b4a92
Dominik Nolte <dominik.nolte@gemedo.de>
2021-04-15 00:08:53 +0200

    feat(database): create model for all social media links

---
+++ src/database/model/social-media.model.ts
@@ -0,0 +1,65 @@
+import { model, Schema, Document } from 'mongoose';
+
+export interface SocialMediaInterface extends Document {
+    website?: string,
+    github?: string,
+    twitter?: string,
+    linkedIn?: string,
+    reddit?: string,
+    telegram?: string,
+    facebook?: string,
+    discord?: string,
+};
+
+const socialMediaSchema: Schema = new Schema({
+    website: {
+        type: String,
+        maxLength: 64,
+        trim: true,
+        lowercase: true
+    },
+    github: {
+        type: String,
+        maxLength: 64,
+        trim: true,
+        lowercase: true
+    },
+    twitter: {
+        type: String,
+        maxLength: 64,
+        trim: true,
+        lowercase: true
+    },
+    linkedIn: {
+        type: String,
+        maxLength: 64,
+        trim: true,
+        lowercase: true
+    },
+    reddit: {
+        type: String,
+        maxLength: 64,
+        trim: true,
+        lowercase: true
+    },
+    telegram: {
+        type: String,
+        maxLength: 64,
+        trim: true,
+        lowercase: true
+    },
+    facebook: {
+        type: String,
+        maxLength: 64,
+        trim: true,
+        lowercase: true
+    },
+    discord: {
+        type: String,
+        maxLength: 64,
+        trim: true,
+        lowercase: true
+    },
+});
+
+export const Company = model('SocialMedia', socialMediaSchema);
