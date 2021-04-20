338ac9e51f110f9eb9ac171ef795f6bf5284aa03
Dominik Nolte <dominik.nolte@gemedo.de>
2021-04-20 23:17:01 +0200

    feat(services): add apis and services to the index files and add services to the ioc config

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,6 +3,12 @@ import { Container } from "typescript-ioc";
 export * from './hello-world.api';
 export * from './hello-world.service';
 
+export * from './project/project.api';
+export * from './project/project.service';
+
+export * from './dao-proposal/dao-proposal.api';
+export * from './dao-proposal/dao-proposal.service';
+
 import config from './ioc.config';
 
 Container.configure(...config);
--- src/services/ioc.config.ts
+++ src/services/ioc.config.ts
@@ -1,13 +1,19 @@
-import {ContainerConfiguration, Scope} from 'typescript-ioc';
-import {HelloWorldApi} from './hello-world.api';
-import {HelloWorldService} from './hello-world.service';
+import { ContainerConfiguration, Scope } from 'typescript-ioc';
+import { ProjectApi, ProjectService } from '.';
+import { DaoProposalApi } from './dao-proposal/dao-proposal.api';
+import { DaoProposalService } from './dao-proposal/dao-proposal.service';
 
 const config: ContainerConfiguration[] = [
-  {
-    bind: HelloWorldApi,
-    to: HelloWorldService,
-    scope: Scope.Singleton
-  }
+    {
+        bind: ProjectApi,
+        to: ProjectService,
+        scope: Scope.Singleton,
+    },
+    {
+        bind: DaoProposalApi,
+        to: DaoProposalService,
+        scope: Scope.Singleton,
+    },
 ];
 
 export default config;
