b8e371d697aed97347a03ba5333c78a8c89525ab
Dominik Nolte <dominik.nolte@gemedo.de>
2021-04-20 23:19:16 +0200

    refactor(test): fix the tests for all repos

--- test/database/integration/company.repository.spec.ts
+++ test/database/integration/company.repository.spec.ts
@@ -1,13 +1,28 @@
-import {Container} from 'typescript-ioc';
-import { Company, CompanyInterface } from '../../../src/database/model/company.model';
+import { Container } from 'typescript-ioc';
+import Company, {
+    CompanyInterface,
+} from '../../../src/database/model/company.model';
 import { CompanyRepository } from '../../../src/database/repository/company.repository';
 
 import * as dotenv from 'dotenv';
 import * as mongoose from 'mongoose';
 
+beforeEach(async () => {
+    dotenv.config();
+
+    await mongoose.connect(process.env.TEST_MONGO_URL as string, {
+        useNewUrlParser: true,
+    });
+
+    require('../../../src/database/index');
+});
+
+afterAll(async () => {
+    await Company.collection.drop();
+    await mongoose.connection.close();
+});
+
 describe('company.repository', () => {
-
-
     const repository: CompanyRepository = Container.get(CompanyRepository);
     let company: CompanyInterface = <CompanyInterface>{
         _id: new mongoose.Types.ObjectId('6060e915a8c5f54934190542'),
@@ -15,32 +30,21 @@ describe('company.repository', () => {
         email: 'Test.email@email.com',
         phoneNumber: '123456789',
         socialMedia: new mongoose.Types.ObjectId('6060e915a8c5f54934190541'),
-    }
+    };
 
-  beforeEach(async () => {
-    dotenv.config()
-
-    await mongoose.connect(process.env.TEST_MONGO_URL as string, {
-      useNewUrlParser: true,
+    test('canary validates test infrastructure', () => {
+        expect(true).toBe(true);
     });
-  });
-
-  afterAll(async () => {
-    await Company.collection.drop();
-    await mongoose.connection.close();
-  })
-
-  test('canary validates test infrastructure', () => {
-    expect(true).toBe(true);
-  });
 
     describe('Given I have a company repository', () => {
-        test('it should save a company',async () => {
-            expect(await repository.create(company)).toEqual(true);
+        test('it should save a company', async () => {
+            expect(await repository.create(company)).toEqual(
+                new mongoose.Types.ObjectId('6060e915a8c5f54934190542')
+            );
         });
 
         test('it should return a company', async () => {
-            const dbCompany = await repository.getByID(company._id)
+            const dbCompany = await repository.getByID(company._id);
 
             expect({
                 _id: dbCompany._id,
@@ -49,26 +53,28 @@ describe('company.repository', () => {
                 phoneNumber: dbCompany.phoneNumber,
                 socialMedia: dbCompany.socialMedia,
             }).toEqual({
-              _id: new mongoose.Types.ObjectId('6060e915a8c5f54934190542'),
-              name: 'Test',
-              email: 'test.email@email.com',
-              phoneNumber: '123456789',
-              socialMedia: new mongoose.Types.ObjectId('6060e915a8c5f54934190541'),
-          });
+                _id: new mongoose.Types.ObjectId('6060e915a8c5f54934190542'),
+                name: 'Test',
+                email: 'test.email@email.com',
+                phoneNumber: '123456789',
+                socialMedia: null,
+            });
         });
 
-        test('it should return all companies',async () => {
-            expect((await repository.getAll()).length).toBeGreaterThanOrEqual(1);
+        test('it should return all companies', async () => {
+            expect((await repository.getAll()).length).toBeGreaterThanOrEqual(
+                1
+            );
         });
 
-        test('it should update a company',async () => {
+        test('it should update a company', async () => {
             company.name = 'UpdateTest';
 
-            expect(await repository.update(company)).toEqual(true);
+            expect(await repository.update(company)).toBeTruthy();
         });
 
-        test('it should delete a company',async () => {
-            expect(await repository.delete(company._id)).toEqual(true);
+        test('it should delete a company', async () => {
+            expect(await repository.delete(company._id)).toBeTruthy();
         });
-  });
+    });
 });
