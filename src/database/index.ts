704e5870aaca7340a4d127c7c832d62db3da0115
Dominik Nolte <dominik.nolte@gemedo.de>
2021-04-20 23:10:20 +0200

    feat(database): create index file that exports all models and repos

---
+++ src/database/index.ts
@@ -0,0 +1,13 @@
+export * from './model/address.model';
+export * from './model/company.model';
+export * from './model/dao-proposal.model';
+export * from './model/job.model';
+export * from './model/project.model';
+export * from './model/social-media.model';
+
+export * from './repository/address.repository'
+export * from './repository/company.repository'
+export * from './repository/dao-proposal.repository'
+export * from './repository/job.repository'
+export * from './repository/project.repository'
+export * from './repository/social-media.repository'
