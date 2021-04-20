d0af2fd604ea102997f43a398c8d1a264089ac11
Dominik Nolte <dominik.nolte@gemedo.de>
2021-04-20 22:40:02 +0200

    feat(controller): create routes for dao-proposals

---
+++ src/controllers/dao-proposal.controller.ts
@@ -0,0 +1,48 @@
+import { Inject } from 'typescript-ioc';
+import { GET, Path, PathParam } from 'typescript-rest';
+import { DaoProposalInterface } from '../database';
+import { LoggerApi } from '../logger';
+import { DaoProposalService } from '../services/dao-proposal/dao-proposal.service';
+
+@Path('/dao-proposal')
+export class ProjectController {
+    @Inject
+    _baseLogger: LoggerApi;
+    @Inject
+    daoProposalService: DaoProposalService;
+    
+    get logger() {
+        return this._baseLogger.child('DaoProposalController');
+    }
+
+    @GET
+    async getProjects(): Promise<DaoProposalInterface[]> {
+        try {
+            return await this.daoProposalService.getDaoProposals();
+        } catch (error: any) {
+            this.logger.error(error);
+        }
+    }
+
+    @Path('/featured')
+    @GET
+    async getFeaturedProjectList(): Promise<DaoProposalInterface[]> {
+        try {
+            return await this.daoProposalService.getFeaturedDaoProposals();
+        } catch (error: any) {
+            this.logger.error(error);
+        }
+    }
+
+    @Path(':id')
+    @GET
+    async getProjectById(
+        @PathParam('id') id: string
+    ): Promise<DaoProposalInterface> {
+        try {
+            return await this.daoProposalService.getDaoProposalById(id);
+        } catch (error: any) {
+            this.logger.error(error);
+        }
+    }
+}
