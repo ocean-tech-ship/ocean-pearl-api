431fd1f1bd22284fa3542e3f6e442d643453d4ab
Dominik Nolte <dominik.nolte@gemedo.de>
2021-05-09 23:56:09 +0200

    feat(database): use the new model for the project repository, add a type for the id, implement the pagination function

--- src/database/repository/project.repository.ts
+++ src/database/repository/project.repository.ts
@@ -1,44 +1,61 @@
-import { FilterQuery, Model } from 'mongoose';
+import { FilterQuery, Model, Types } from 'mongoose';
 import { MongooseDeleteResponseInterface } from '../interfaces/mongoose-delete-response.interface';
 import { RepositoryInterface } from '../interfaces/repository.inteface';
-import Project, { ProjectInterface } from '../model/project.model';
+import Project, { ProjectInterface, ProjectType } from '../model/project.model';
 
-export class ProjectRepository
-    implements RepositoryInterface<ProjectInterface> {
-    private model: Model<ProjectInterface>;
+export class ProjectRepository implements RepositoryInterface<ProjectType> {
+    private model: Model<ProjectType>;
 
     constructor() {
         this.model = Project;
     }
 
-    public async getByID(id: string): Promise<ProjectInterface> {
+    public async getByID(id: Types.ObjectId): Promise<ProjectType> {
         try {
             return await this.model
                 .findById(id)
                 .populate('company')
                 .populate('daoProposals')
-                .populate('socialMedia');
+                .populate({
+                    path: 'socialMedia',
+                    options: { lean: true },
+                });
         } catch (error: any) {
             throw error;
         }
     }
 
-    public async getAll(query?: FilterQuery<any>): Promise<ProjectInterface[]> {
+    public async getAll(query?: FilterQuery<any>): Promise<ProjectType[]> {
         try {
             return await this.model
                 .find(query || {})
                 .populate('company')
                 .populate('daoProposals')
-                .populate('socialMedia');
+                .populate({
+                    path: 'socialMedia',
+                    options: { lean: true },
+                });
         } catch (error: any) {
             throw error;
         }
     }
 
-    public async getPaginated(): Promise<ProjectInterface[]> {
+    public async getPaginated(
+        page: number,
+        limit: number,
+        query?: FilterQuery<any>
+    ): Promise<ProjectType[]> {
         try {
-
-            return 
+            return await this.model
+                .find(query || {})
+                .skip(page - 1 * limit)
+                .limit(limit)
+                .populate('company')
+                .populate('daoProposals')
+                .populate({
+                    path: 'socialMedia',
+                    options: { lean: true },
+                });
         } catch (error: any) {
             throw error;
         }
@@ -46,7 +63,7 @@ export class ProjectRepository
 
     public async update(model: ProjectInterface): Promise<boolean> {
         try {
-            const response: ProjectInterface = await this.model.findOneAndUpdate(
+            const response: ProjectType = await this.model.findOneAndUpdate(
                 { _id: model._id },
                 model
             );
@@ -57,7 +74,7 @@ export class ProjectRepository
         }
     }
 
-    public async create(model: ProjectInterface): Promise<string> {
+    public async create(model: ProjectInterface): Promise<Types.ObjectId> {
         try {
             const response: ProjectInterface = await this.model.create(model);
 
@@ -67,7 +84,7 @@ export class ProjectRepository
         }
     }
 
-    public async delete(id: string): Promise<boolean> {
+    public async delete(id: Types.ObjectId): Promise<boolean> {
         try {
             const response: MongooseDeleteResponseInterface = await this.model.deleteOne(
                 { _id: id }
