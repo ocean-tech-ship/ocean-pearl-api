d72ce8bc6ba29e2228bfd7ae41502f53252e4d75
Dominik Nolte <dominik.nolte@gemedo.de>
2021-05-10 00:07:20 +0200

    refactor(services): change the function signature for the detail call

--- src/services/project/project.api.ts
+++ src/services/project/project.api.ts
@@ -1,7 +1,8 @@
+import { Types } from 'mongoose';
 import { ProjectInterface } from "../../database/model/project.model";
 
 export abstract class ProjectApi {
     abstract getProjects(): Promise<ProjectInterface[]>;
     abstract getFeaturedProjects(): Promise<ProjectInterface[]>;
-    abstract getProjectById(id: string): Promise<ProjectInterface>;
+    abstract getProjectById(id: Types.ObjectId): Promise<ProjectInterface>;
 }
