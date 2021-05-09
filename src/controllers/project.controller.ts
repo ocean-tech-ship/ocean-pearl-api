113eeaf24e890c07da226e282dc7067f328bddef
Dominik Nolte <dominik.nolte@gemedo.de>
2021-05-10 00:04:48 +0200

    refactor(controller): fix wording for the rooutes, create an objectId for the detail calls

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -1,8 +1,9 @@
-import { ProjectInterface } from '../database/model/project.model';
 import { ProjectService } from '../services/project/project.service';
 import { GET, Path, PathParam } from 'typescript-rest';
 import { Inject } from 'typescript-ioc';
 import { LoggerApi } from '../logger';
+import { ProjectInterface } from '../database';
+import { Types } from 'mongoose';
 
 @Path('/project')
 export class ProjectController {
@@ -10,13 +11,13 @@ export class ProjectController {
     _baseLogger: LoggerApi;
     @Inject
     projectService: ProjectService;
-    
+
     get logger() {
         return this._baseLogger.child('ProjectController');
     }
 
     @GET
-    async getProjects(): Promise<ProjectInterface[]> {
+    async getProjects(): Promise<Array<ProjectInterface>> {
         try {
             return await this.projectService.getProjects();
         } catch (error: any) {
@@ -24,9 +25,9 @@ export class ProjectController {
         }
     }
 
-    @Path('/featured')
+    @Path('featured')
     @GET
-    async getFeaturedProjectList(): Promise<ProjectInterface[]> {
+    async getFeaturedProjectList(): Promise<Array<ProjectInterface>> {
         try {
             return await this.projectService.getFeaturedProjects();
         } catch (error: any) {
@@ -34,13 +35,15 @@ export class ProjectController {
         }
     }
 
-    @Path(':id')
+    @Path('detail/:id')
     @GET
     async getProjectById(
         @PathParam('id') id: string
     ): Promise<ProjectInterface> {
         try {
-            return await this.projectService.getProjectById(id);
+            return await this.projectService.getProjectById(
+                new Types.ObjectId(id)
+            );
         } catch (error: any) {
             this.logger.error(error);
         }
