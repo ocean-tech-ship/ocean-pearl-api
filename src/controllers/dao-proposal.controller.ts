9fb44fbeaa5dc6ec281434fb76ec0bc41c393b29
Dominik Nolte <dominik.nolte@gemedo.de>
2021-05-10 00:04:48 +0200

    refactor(controller): fix wording for the rooutes, create an objectId for the detail calls

--- src/controllers/dao-proposal.controller.ts
+++ src/controllers/dao-proposal.controller.ts
@@ -1,3 +1,4 @@
+import { Types } from 'mongoose';
 import { Inject } from 'typescript-ioc';
 import { GET, Path, PathParam } from 'typescript-rest';
 import { DaoProposalInterface } from '../database';
@@ -10,7 +11,7 @@ export class DaoProposalController {
     _baseLogger: LoggerApi;
     @Inject
     daoProposalService: DaoProposalService;
-    
+
     get logger() {
         return this._baseLogger.child('DaoProposalController');
     }
@@ -24,7 +25,7 @@ export class DaoProposalController {
         }
     }
 
-    @Path('/featured')
+    @Path('featured')
     @GET
     async getFeaturedProjectList(): Promise<DaoProposalInterface[]> {
         try {
@@ -34,13 +35,15 @@ export class DaoProposalController {
         }
     }
 
-    @Path(':id')
+    @Path('detail/:id')
     @GET
     async getProjectById(
         @PathParam('id') id: string
     ): Promise<DaoProposalInterface> {
         try {
-            return await this.daoProposalService.getDaoProposalById(id);
+            return await this.daoProposalService.getDaoProposalById(
+                new Types.ObjectId(id)
+            );
         } catch (error: any) {
             this.logger.error(error);
         }
