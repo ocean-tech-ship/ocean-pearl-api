c147c7f6c3d522ddf5b8fd958fb54c056ac0c996
Dominik Nolte <dominik.nolte@gemedo.de>
2021-05-10 00:07:02 +0200

    refactor(services): change the function signature for the detail call

--- src/services/dao-proposal/dao-proposal.api.ts
+++ src/services/dao-proposal/dao-proposal.api.ts
@@ -1,7 +1,8 @@
+import { Types } from 'mongoose';
 import { DaoProposalInterface } from '../../database/model/dao-proposal.model';
 
 export abstract class DaoProposalApi{
     abstract getDaoProposals(): Promise<DaoProposalInterface[]>;
     abstract getFeaturedDaoProposals(): Promise<DaoProposalInterface[]>;
-    abstract getDaoProposalById(id: string): Promise<DaoProposalInterface>;
+    abstract getDaoProposalById(id: Types.ObjectId): Promise<DaoProposalInterface>;
 }
