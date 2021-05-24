bf874b052cff8a2e6c94f556a1bfc4914eedc611
Dominik Nolte <dominik.nolte@gemedo.de>
2021-05-24 20:51:41 +0200

    feat(database): create a repo for the ocean-users that implements the repository interface

---
+++ src/database/repository/ocean-user.repository.ts
@@ -0,0 +1,67 @@
+import { FilterQuery, Model, Types } from 'mongoose';
+import { MongooseDeleteResponseInterface } from '../interfaces/mongoose-delete-response.interface';
+import { RepositoryInterface } from '../interfaces/repository.inteface';
+import OceanUser, {
+    OceanUserInterface,
+    OceanUserType,
+} from '../model/ocean-user.model';
+
+export class JobRepository implements RepositoryInterface<OceanUserType> {
+    private model: Model<OceanUserType>;
+
+    constructor() {
+        this.model = OceanUser;
+    }
+
+    public async getByID(id: Types.ObjectId): Promise<OceanUserType> {
+        try {
+            return await this.model.findById(id);
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async getAll(query?: FilterQuery<any>): Promise<OceanUserType[]> {
+        try {
+            return await this.model.find(query || {});
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async update(model: OceanUserInterface): Promise<boolean> {
+        try {
+            const response: OceanUserInterface =
+                await this.model.findOneAndUpdate({ _id: model._id }, model);
+
+            return response !== null;
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async create(model: OceanUserInterface): Promise<Types.ObjectId> {
+        try {
+            const response: OceanUserInterface = await this.model.create(model);
+
+            return response._id;
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async delete(id: Types.ObjectId): Promise<boolean> {
+        try {
+            const response: MongooseDeleteResponseInterface =
+                await this.model.deleteOne({ _id: id });
+
+            return response.deletedCount === 1;
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public getModel(): Model<OceanUserType> {
+        return this.model;
+    }
+}
