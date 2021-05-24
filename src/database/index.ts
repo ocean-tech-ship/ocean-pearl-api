84b8c1ce48198e8e10cccbefe2fb1eaf40187034
Dominik Nolte <dominik.nolte@gemedo.de>
2021-05-24 21:03:50 +0200

    feat(database): add all new Models and Repos to the index file

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,7 +3,9 @@ export * from './model/company.model';
 export * from './model/dao-proposal.model';
 export * from './model/job.model';
 export * from './model/project.model';
-export * from './model/social-media.model';
+export * from './model/ocean-user.model';
+export * from './model/kpi-target.model';
+export * from './model/deliverable.model';
 
 export * from './repository/address.repository'
 export * from './repository/company.repository'
@@ -11,3 +13,4 @@ export * from './repository/dao-proposal.repository'
 export * from './repository/job.repository'
 export * from './repository/project.repository'
 export * from './repository/social-media.repository'
+export * from './repository/ocean-user.repository'
