928b6be2f5ec3738f27b39dd4d974a9c33e64b2a
Dominik Nolte <dominik.nolte@gemedo.de>
2021-05-27 21:32:38 +0200

    refactor(controller): import and use the new commands

--- src/controllers/project.controller.ts
+++ src/controllers/project.controller.ts
@@ -1,16 +1,20 @@
-import { ProjectService } from '../services/project/project.service';
 import { GET, Path, PathParam } from 'typescript-rest';
 import { Inject } from 'typescript-ioc';
 import { LoggerApi } from '../logger';
 import { ProjectInterface } from '../database';
 import { Types } from 'mongoose';
+import { GetFeaturedProjectsCommand, GetProjectByIdCommand, GetProjectsCommand } from '../services';
 
 @Path('/project')
 export class ProjectController {
     @Inject
     _baseLogger: LoggerApi;
     @Inject
-    projectService: ProjectService;
+    getProjectsCommand: GetProjectsCommand;
+    @Inject
+    getFeaturedProjectsCommand: GetFeaturedProjectsCommand;
+    @Inject
+    getProjectByIdCommand: GetProjectByIdCommand;
 
     get logger() {
         return this._baseLogger.child('ProjectController');
@@ -19,7 +23,7 @@ export class ProjectController {
     @GET
     async getProjects(): Promise<Array<ProjectInterface>> {
         try {
-            return await this.projectService.getProjects();
+            return await this.getProjectsCommand.execute();
         } catch (error: any) {
             this.logger.error(error);
         }
@@ -27,12 +31,18 @@ export class ProjectController {
 
     @Path('featured')
     @GET
-    async getFeaturedProjectList(): Promise<Array<ProjectInterface>> {
+    async getFeaturedProjectList(): Promise<ProjectInterface[]> {
         try {
-            return await this.projectService.getFeaturedProjects();
+            return await this.getFeaturedProjectsCommand.execute();
         } catch (error: any) {
             this.logger.error(error);
         }
+    }
+
+    @Path('latest')
+    @GET
+    async getLatestProjects(): Promise<ProjectInterface[]> {
+        return;
     }
 
     @Path('detail/:id')
@@ -41,7 +51,7 @@ export class ProjectController {
         @PathParam('id') id: string
     ): Promise<ProjectInterface> {
         try {
-            return await this.projectService.getProjectById(
+            return await this.getProjectByIdCommand.execute(
                 new Types.ObjectId(id)
             );
         } catch (error: any) {
