f6f28c370f359725c50f68a86f634bea5d70e202
Dominik Nolte <dominik.nolte@gemedo.de>
2021-05-27 23:45:51 +0200

    refactor(services): delete the unused DaoProposalService

--- src/services/dao-proposal/dao-proposal.api.ts
+++
@@ -1,8 +0,0 @@
-import { Types } from 'mongoose';
-import { DaoProposalInterface } from '../../database/model/dao-proposal.model';
-
-export abstract class DaoProposalApi{
-    abstract getDaoProposals(): Promise<DaoProposalInterface[]>;
-    abstract getFeaturedDaoProposals(): Promise<DaoProposalInterface[]>;
-    abstract getDaoProposalById(id: Types.ObjectId): Promise<DaoProposalInterface>;
-}
