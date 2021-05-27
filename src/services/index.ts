60500840ca486049c5660c3d93ca8bbbdd62ad18
Dominik Nolte <dominik.nolte@gemedo.de>
2021-05-27 21:29:15 +0200

    refactor(services): remove the old service and import the new commands

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,8 +3,12 @@ import { Container } from "typescript-ioc";
 export * from './hello-world.api';
 export * from './hello-world.service';
 
-export * from './project/project.api';
-export * from './project/project.service';
+export * from './project/api/getFeaturedProjectsCommand.api';
+export * from './project/api/getProjectByIdCommand.api';
+export * from './project/api/getProjectsCommand.api';
+export * from './project/command/get-featured-projects.command';
+export * from './project/command/get-project-by-id.command';
+export * from './project/command/get-projects.command';
 
 export * from './dao-proposal/dao-proposal.api';
 export * from './dao-proposal/dao-proposal.service';
