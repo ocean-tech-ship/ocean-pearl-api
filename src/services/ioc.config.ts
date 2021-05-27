dbdaaf5065f5a1004a2f77f698527bbea093bb48
Dominik Nolte <dominik.nolte@gemedo.de>
2021-05-27 21:30:12 +0200

    refactor(services): remove the project service from the IoC config and add the new commands

--- src/services/ioc.config.ts
+++ src/services/ioc.config.ts
@@ -1,12 +1,29 @@
 import { ContainerConfiguration, Scope } from 'typescript-ioc';
-import { ProjectApi, ProjectService } from '.';
+import {
+    GetFeaturedProjectsCommand,
+    GetFeaturedProjectsCommandApi,
+    GetProjectByIdCommand,
+    GetProjectByIdCommandApi,
+} from '.';
 import { DaoProposalApi } from './dao-proposal/dao-proposal.api';
 import { DaoProposalService } from './dao-proposal/dao-proposal.service';
+import { GetProjectsCommandApi } from './project/api/getProjectsCommand.api';
+import { GetProjectsCommand } from './project/command/get-projects.command';
 
 const config: ContainerConfiguration[] = [
     {
-        bind: ProjectApi,
-        to: ProjectService,
+        bind: GetProjectsCommandApi,
+        to: GetProjectsCommand,
+        scope: Scope.Singleton,
+    },
+    {
+        bind: GetFeaturedProjectsCommandApi,
+        to: GetFeaturedProjectsCommand,
+        scope: Scope.Singleton,
+    },
+    {
+        bind: GetProjectByIdCommandApi,
+        to: GetProjectByIdCommand,
         scope: Scope.Singleton,
     },
     {
