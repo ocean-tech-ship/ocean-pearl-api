d951b903f69acadf8ac684f3f78ece445448ef88
Dominik Nolte <dominik.nolte@gemedo.de>
2021-05-27 21:27:51 +0200

    refactor(services): remove the unified project service

--- src/services/project/project.api.ts
+++
@@ -1,8 +0,0 @@
-import { Types } from 'mongoose';
-import { ProjectInterface } from "../../database/model/project.model";
-
-export abstract class ProjectApi {
-    abstract getProjects(): Promise<ProjectInterface[]>;
-    abstract getFeaturedProjects(): Promise<ProjectInterface[]>;
-    abstract getProjectById(id: Types.ObjectId): Promise<ProjectInterface>;
-}
