fea6e36d4de0f00711157ab8c1df8533b36c858f
Dominik Nolte <dominik.nolte@gemedo.de>
2021-05-29 00:24:34 +0200

    feat(services): add new commands to the IoC Container config

--- src/services/index.ts
+++ src/services/index.ts
@@ -6,16 +6,25 @@ export * from './hello-world.service';
 export * from './project/api/get-featured-projects-command.api';
 export * from './project/api/get-project-by-id-command.api';
 export * from './project/api/get-projects-command.api';
+export * from './project/api/get-latest-projects-command.api';
+export * from './project/api/get-dao-featured-projects-comand.api';
+export * from './project/api/get-projects-paginated-command.api';
 export * from './project/command/get-featured-projects.command';
 export * from './project/command/get-project-by-id.command';
 export * from './project/command/get-projects.command';
+export * from './project/command/get-latest-projects.command';
+export * from './project/command/get-dao-featured-projects.command';
+export * from './project/command/get-projects-paginated.command';
 
 export * from './dao-proposal/api/get-dao-proposals-command.api';
 export * from './dao-proposal/api/get-doa-proposals-by-id-command.api';
-export * from './dao-proposal/api/get-featured-dao-proposals-comand.api';
+export * from './dao-proposal/api/get-open-dao-proposals-comand.api';
 export * from './dao-proposal/command/get-dao-proposals.command';
 export * from './dao-proposal/command/get-doa-proposal-by-id.command';
-export * from './dao-proposal/command/get-featured-doa-proposals.command';
+export * from './dao-proposal/command/get-open-doa-proposals.command';
+
+export * from './metrics/api/calculate-metrics-command.api';
+export * from './metrics/command/calculate-metrics.command';
 
 import config from './ioc.config';
 
--- src/services/ioc.config.ts
+++ src/services/ioc.config.ts
@@ -1,23 +1,35 @@
 import { ContainerConfiguration, Scope } from 'typescript-ioc';
-import {
-    GetFeaturedProjectsCommand,
-    GetFeaturedProjectsCommandApi,
-    GetProjectByIdCommand,
-    GetProjectByIdCommandApi,
-} from '.';
 import { GetDaoProposalsCommandApi } from './dao-proposal/api/get-dao-proposals-command.api';
 import { GetDaoProposalByIdCommandApi } from './dao-proposal/api/get-doa-proposals-by-id-command.api';
-import { GetFeaturedDaoProposalsCommandApi } from './dao-proposal/api/get-featured-dao-proposals-comand.api';
+import { GetOpenDaoProposalsCommandApi } from './dao-proposal/api/get-open-dao-proposals-comand.api';
 import { GetDaoProposalsCommand } from './dao-proposal/command/get-dao-proposals.command';
 import { GetDaoProposalByIdCommand } from './dao-proposal/command/get-doa-proposal-by-id.command';
-import { GetFeaturedDaoProposalsCommand } from './dao-proposal/command/get-featured-doa-proposals.command';
+import { GetOpenDaoProposalsCommand } from './dao-proposal/command/get-open-doa-proposals.command';
+import { CalculateMetricsCommandApi } from './metrics/api/calculate-metrics-command.api';
+import { CalculateMetricsCommand } from './metrics/command/calculate-metrics.command';
+import { GetDaoFeaturedProjectsCommandApi } from './project/api/get-dao-featured-projects-comand.api';
+import { GetFeaturedProjectsCommandApi } from './project/api/get-featured-projects-command.api';
+import { GetLatestProjectsComandApi } from './project/api/get-latest-projects-command.api';
+import { GetProjectByIdCommandApi } from './project/api/get-project-by-id-command.api';
 import { GetProjectsCommandApi } from './project/api/get-projects-command.api';
+import { GetProjectsPaginatedCommandApi } from './project/api/get-projects-paginated-command.api';
+import { GetDaoFeaturedProjectsCommand } from './project/command/get-dao-featured-projects.command';
+import { GetFeaturedProjectsCommand } from './project/command/get-featured-projects.command';
+import { GetLatestProjectsComand } from './project/command/get-latest-projects.command';
+import { GetProjectByIdCommand } from './project/command/get-project-by-id.command';
+import { GetProjectsPaginatedCommand } from './project/command/get-projects-paginated.command';
 import { GetProjectsCommand } from './project/command/get-projects.command';
 
 const config: ContainerConfiguration[] = [
+    // Projects
     {
         bind: GetProjectsCommandApi,
         to: GetProjectsCommand,
+        scope: Scope.Singleton,
+    },
+    {
+        bind: GetProjectsPaginatedCommandApi,
+        to: GetProjectsPaginatedCommand,
         scope: Scope.Singleton,
     },
     {
@@ -31,18 +43,35 @@ const config: ContainerConfiguration[] = [
         scope: Scope.Singleton,
     },
     {
+        bind: GetLatestProjectsComandApi,
+        to: GetLatestProjectsComand,
+        scope: Scope.Singleton,
+    },
+    {
+        bind: GetDaoFeaturedProjectsCommandApi,
+        to: GetDaoFeaturedProjectsCommand,
+        scope: Scope.Singleton,
+    },
+    // Dao Proposals
+    {
         bind: GetDaoProposalsCommandApi,
         to: GetDaoProposalsCommand,
         scope: Scope.Singleton,
     },
     {
-        bind: GetFeaturedDaoProposalsCommandApi,
-        to: GetFeaturedDaoProposalsCommand,
+        bind: GetOpenDaoProposalsCommandApi,
+        to: GetOpenDaoProposalsCommand,
         scope: Scope.Singleton,
     },
     {
         bind: GetDaoProposalByIdCommandApi,
         to: GetDaoProposalByIdCommand,
+        scope: Scope.Singleton,
+    },
+    // Metrics
+    {
+        bind: CalculateMetricsCommandApi,
+        to: CalculateMetricsCommand,
         scope: Scope.Singleton,
     },
 ];
