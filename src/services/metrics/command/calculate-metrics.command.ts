0bccaeb31817582563ebf5889a0213d6f71a033e
Dominik Nolte <dominik.nolte@gemedo.de>
2021-05-31 02:54:52 +0200

    refactor(services): use proposals of one round to calculate the metrics

--- src/services/metrics/command/calculate-metrics.command.ts
+++ src/services/metrics/command/calculate-metrics.command.ts
@@ -1,24 +1,24 @@
 import { Inject } from 'typescript-ioc';
-import { GetOpenDaoProposalsCommand } from '../../dao-proposal/command/get-open-doa-proposals.command';
+import { GetDaoProposalsByRoundCommand } from '../../dao-proposal/command/get-dao-proposals-by-round.command';
 import { CalculateMetricsCommandApi } from '../api/calculate-metrics-command.api';
 import { MetricsInterface } from '../interface/metrics.interface';
 
-const CURRENT_ROUND: number = 5;
+const CURRENT_ROUND: number = 6;
 
 export class CalculateMetricsCommand implements CalculateMetricsCommandApi {
-    getOpenDaoProposalsComand: GetOpenDaoProposalsCommand;
+    getDaoProposalsByRoundCommand: GetDaoProposalsByRoundCommand;
 
     constructor(
         @Inject
-        getOpenDaoProposalsComand: GetOpenDaoProposalsCommand
+        getDaoProposalsByRoundCommand: GetDaoProposalsByRoundCommand
     ) {
-        this.getOpenDaoProposalsComand = getOpenDaoProposalsComand;
+        this.getDaoProposalsByRoundCommand = getDaoProposalsByRoundCommand;
     }
 
     public async execute(): Promise<MetricsInterface> {
         let totalVotesCount: number = 0;
         let totalRequestedFunding: number = 0;
-        const daoProposals = await this.getOpenDaoProposalsComand.execute(CURRENT_ROUND);
+        const daoProposals = await this.getDaoProposalsByRoundCommand.execute(CURRENT_ROUND);
 
         for (const proposal of daoProposals) {
             totalVotesCount += proposal.votes;
