f19f5d942fab6e54f460313d39e3c6d99b274844
Dominik Nolte <dominik.nolte@gemedo.de>
2021-07-12 00:49:17 +0200

    feat(database): add an enum for the fundamental metrics options

---
+++ src/database/enums/fundamental-metric.enum.ts
@@ -0,0 +1,10 @@
+export enum FundamentalMetricEnum {
+    DataConsumeVolume = 'dataConsumeVolume',
+    NetworkRevenue = 'networkRevenue',
+    Tvl = 'tvl',
+    MarketMonthlyRevenue = 'marketMonthlyRevenue',
+    DatatokenContracts = 'datatokenContracts',
+    MarketWau = 'marketWau',
+    D90Retention = 'd90Retention',
+    MvpLaunch = 'mvpLaunch',
+}
