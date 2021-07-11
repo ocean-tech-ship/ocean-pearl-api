8e692d51c5668a1f6b8c8fb79828652758979562
Dominik Nolte <dominik.nolte@gemedo.de>
2021-07-12 00:57:35 +0200

    refactor(database): update the new types from the schemas

---
+++ src/database/repositories/dao-proposal.repository.ts
@@ -0,0 +1,145 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { FindQuery } from '../interfaces/find-query.interface';
+import { MongooseDeleteResponse } from '../interfaces/mongoose-delete-response.interface';
+import { PaginationOptions } from '../interfaces/pagination-options.interface';
+import { RepositoryInterface } from '../interfaces/repository.inteface';
+import { DaoProposal, DaoProposalType } from '../schemas/dao-proposal.schema';
+
+@Injectable()
+export class DaoProposalRepository
+    implements RepositoryInterface<DaoProposalType> {
+    constructor(
+        @InjectModel('DaoProposal') private model: Model<DaoProposalType>,
+    ) {}
+
+    public async getByID(id: string): Promise<DaoProposal> {
+        try {
+            return await this.model
+                .findOne({ id: id })
+                .lean()
+                .populate({
+                    path: 'project',
+                    select: '-daoProposals -_id -__v',
+                })
+                .populate({
+                    path: 'deliverables',
+                    select: '-_id -__v',
+                })
+                .populate({
+                    path: 'kpiTargets',
+                    select: '-_id -__v',
+                })
+                .populate({
+                    path: 'fundingRound',
+                    select: '-_id -__v',
+                })
+                .select('-_id -__v')
+                .exec();
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async getAll(query?: FindQuery): Promise<DaoProposal[]> {
+        try {
+            return await this.model
+                .find(query?.find || {})
+                .sort(query?.sort || {})
+                .lean()
+                .populate({
+                    path: 'project',
+                    select: '-daoProposals -_id -__v',
+                })
+                .populate({
+                    path: 'deliverables',
+                    select: '-_id -__v',
+                })
+                .populate({
+                    path: 'kpiTargets',
+                    select: '-_id -__v',
+                })
+                .populate({
+                    path: 'fundingRound',
+                    select: '-_id -__v',
+                })
+                .select('-_id -__v')
+                .exec();
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async getPaginated(
+        options: PaginationOptions,
+    ): Promise<DaoProposal[]> {
+        try {
+            return await this.model
+                .find(options.find || {})
+                .sort(options.sort || {})
+                .skip((options.page - 1) * options.limit)
+                .limit(options.limit)
+                .lean()
+                .populate({
+                    path: 'project',
+                    select: '-daoProposals -_id -__v',
+                })
+                .populate({
+                    path: 'deliverables',
+                    select: '-_id -__v',
+                })
+                .populate({
+                    path: 'kpiTargets',
+                    select: '-_id -__v',
+                })
+                .populate({
+                    path: 'fundingRound',
+                    select: '-_id -__v',
+                })
+                .select('-_id -__v')
+                .exec();
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async update(model: DaoProposal): Promise<boolean> {
+        try {
+            const response: DaoProposal = await this.model.findOneAndUpdate(
+                { id: model.id },
+                model,
+            );
+
+            return response !== null;
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async create(model: DaoProposal): Promise<Types.ObjectId> {
+        try {
+            const response: DaoProposal = await this.model.create(model);
+
+            return response._id;
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async delete(id: string): Promise<boolean> {
+        try {
+            const response: MongooseDeleteResponse = await this.model.deleteOne(
+                { id: id },
+            );
+
+            return response.deletedCount === 1;
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public getModel(): Model<DaoProposalType> {
+        return this.model;
+    }
+}
---
+++ src/database/repositories/job.repository.ts
@@ -0,0 +1,82 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { FilterQuery, Model, Types } from 'mongoose';
+import { MongooseDeleteResponse } from '../interfaces/mongoose-delete-response.interface';
+import { RepositoryInterface } from '../interfaces/repository.inteface';
+import { Job, JobType } from '../schemas/job.schema';
+
+@Injectable()
+export class JobRepository implements RepositoryInterface<JobType> {
+    constructor(@InjectModel('Job') private model: Model<JobType>) {}
+
+    public async getByID(id: string): Promise<Job> {
+        try {
+            return await this.model
+                .findOne({ id: id })
+                .lean()
+                .populate({
+                    path: 'company',
+                    select: 'id',
+                })
+                .select('-_id -__v')
+                .exec();
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async getAll(query?: FilterQuery<any>): Promise<Job[]> {
+        try {
+            return await this.model
+                .find(query || {})
+                .lean()
+                .populate({
+                    path: 'company',
+                    select: 'id',
+                })
+                .select('-_id -__v')
+                .exec();
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async update(model: Job): Promise<boolean> {
+        try {
+            const response: Job = await this.model.findOneAndUpdate(
+                { id: model.id },
+                model,
+            );
+
+            return response !== null;
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async create(model: Job): Promise<Types.ObjectId> {
+        try {
+            const response: Job = await this.model.create(model);
+
+            return response._id;
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public async delete(id: string): Promise<boolean> {
+        try {
+            const response: MongooseDeleteResponse = await this.model.deleteOne(
+                { id: id },
+            );
+
+            return response.deletedCount === 1;
+        } catch (error: any) {
+            throw error;
+        }
+    }
+
+    public getModel(): Model<JobType> {
+        return this.model;
+    }
+}
