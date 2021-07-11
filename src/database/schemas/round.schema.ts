a826ac8a00c00a12264ab514c1167c0f9a27f19e
Dominik Nolte <dominik.nolte@gemedo.de>
2021-07-12 00:51:46 +0200

    feat(database): add a new class/schema for rounds

---
+++ src/database/schemas/round.schema.ts
@@ -0,0 +1,65 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+import { nanoid } from '../functions/nano-id.function';
+
+export type RoundType = Round & Document;
+
+@Schema({ timestamps: true })
+export class Round {
+    _id: Types.ObjectId;
+
+    @Prop({
+        type: String,
+        default: () => nanoid(),
+        unique: true,
+    })
+    id: string;
+
+    @Prop({
+        type: Number,
+        min: 0,
+        unique: true,
+    })
+    round: number;
+
+    @Prop({
+        type: Number,
+        min: 0,
+        default: 0,
+    })
+    maxGrant: number;
+
+    @Prop({
+        type: Number,
+        min: 0,
+        default: 0,
+    })
+    earmarked: number;
+
+    @Prop({
+        type: Number,
+        min: 0,
+        default: 0,
+    })
+    availableFunding: number;
+
+    @Prop({
+        type: Date,
+        required: true,
+    })
+    submissionEndDate: Date;
+
+    @Prop({
+        type: Date,
+        required: true,
+    })
+    votingStartDate: Date;
+
+    @Prop({
+        type: Date,
+        required: true,
+    })
+    votingEndDate: Date;
+}
+
+export const RoundSchema = SchemaFactory.createForClass(Round);
