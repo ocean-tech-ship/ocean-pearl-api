47031de4bf67d84a42a7f837211d845e86a4e86b
Dominik Nolte <dominik.nolte@gemedo.de>
2021-07-12 00:45:06 +0200

    feat(global): add nestjs base structure

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { ProjectsModule } from './projects/projects.module';
+import { DaoProposalsModule } from './dao-proposals/dao-proposals.module';
+import { MetricsModule } from './metrics/metrics.module';
+import { PagesModule } from './pages/pages.module';
+import { AirtableModule } from './airtable/airtable.module';
+import { ScheduleModule } from '@nestjs/schedule';
+
+require('dotenv').config({ path: `./enviroment/${process.env.NODE_ENV}.env` });
+
+@Module({
+    imports: [
+        MongooseModule.forRoot(process.env.MONGO_URL as string, {
+            useCreateIndex: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            bufferCommands: false,
+            bufferMaxEntries: 0,
+        }),
+        ScheduleModule.forRoot(),
+        DatabaseModule,
+        ProjectsModule,
+        DaoProposalsModule,
+        MetricsModule,
+        PagesModule,
+        AirtableModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder()
+        .setTitle('Ocean Pearl API')
+        .setDescription('Public API endpoint of the Ocean-Pearl project')
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(3000);
+    console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
