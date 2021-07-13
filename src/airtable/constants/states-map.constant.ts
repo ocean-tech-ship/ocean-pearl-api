83a7cfe8c8b48ccc8da878093a1f821df57e5471
Dominik Nolte <dominik.nolte@gemedo.de>
2021-07-13 22:17:26 +0200

    refactor(airtable): move existing maps into constants

---
+++ src/airtable/constants/states-map.constant.ts
@@ -0,0 +1,12 @@
+import { DaoProposalStatusEnum } from '../../database/enums/dao-proposal-status.enum';
+
+export const StatesMap = {
+    'Running': DaoProposalStatusEnum.Running,
+    'Not Granted': DaoProposalStatusEnum.NotGranted,
+    'Granted': DaoProposalStatusEnum.Granted,
+    'Funded': DaoProposalStatusEnum.Funded,
+    'Not Funded': DaoProposalStatusEnum.NotFunded,
+    'Rejected': DaoProposalStatusEnum.Rejected,
+    'Received': DaoProposalStatusEnum.Received,
+    'Ended': DaoProposalStatusEnum.Ended,
+};
