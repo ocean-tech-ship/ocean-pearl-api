504d21ee517a29ff16dc5a78dd17f8dc23ff3108
Dominik Nolte <dominik.nolte@gemedo.de>
2021-08-01 20:24:37 +0200

    feat(airtable): update the import scripts to reflect updated names and new fields in airtable

--- src/airtable/services/sync-rounds-data.service.ts
+++ src/airtable/services/sync-rounds-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
 import { AxiosResponse } from 'axios';
-import { CurrencyEnum } from '../../database/enums/currency.enum';
+import { PaymentOptionEnum } from '../../database/enums/payment-option.enum';
 import { FindQuery } from '../../database/interfaces/find-query.interface';
 import { RoundRepository } from '../../database/repositories/round.repository';
 import { Round } from '../../database/schemas/round.schema';
@@ -21,42 +21,44 @@ export class SyncRoundsDataService {
         timeZone: 'Europe/Berlin',
     })
     public async execute(): Promise<void> {
-        this.logger.log('Start syncing Rounds Job.');
+        this.logger.log('Start syncing Rounds from Airtable Job.');
 
         const roundsResponse: AxiosResponse = await this.roundsProvider.fetch();
         const airtbaleRounds = roundsResponse.data.records;
 
         for (let round of airtbaleRounds) {
-            round = round.fields;
-
-            const newRound: Round = this.mapRound(round);
+            const newRound: Round = this.mapRound(round.fields);
 
             this.syncRound(newRound);
         }
 
-        this.logger.log('Finish syncing Rounds Job.');
+        this.logger.log('Finish syncing Rounds from Airtable Job.');
     }
 
     private mapRound(round: any): Round {
         let mappedRound: Round = {
-            round: round.name.split(' ')[1],
-            earmarked: round.Earmarked ?? round['Earmarked USD'],
+            round: round['Round'].split(' ')[1],
+            earmarked: round['Earmarked'] ?? round['Earmarked USD'],
             maxGrant: round['Max Grant'] ?? round['Max Grant USD'],
-            grantCurrency: round['Max Grant']
-                ? CurrencyEnum.Ocean
-                : CurrencyEnum.Usd,
+            paymentOption: round['Max Grant']
+                ? PaymentOptionEnum.Ocean
+                : PaymentOptionEnum.Usd,
             availableFunding:
                 round['Funding Available'] ?? round['Funding Available USD'],
+            startDate: round['Start Date'] ? new Date(round['Start Date']) : null,
             submissionEndDate: new Date(round['Proposals Due By']),
+            votingStartDate: round['Voting Starts'] ? new Date(round['Voting Starts']) : null,
             votingEndDate: new Date(round['Voting Ends']),
         } as Round;
 
-        const startDate: Date = new Date(round['Proposals Due By']);
+        if (!mappedRound.votingStartDate) {
+            const startDate: Date = new Date(round['Proposals Due By']);
 
-        startDate.setDate(startDate.getDate() + 1);
-        startDate.setUTCHours(0, 0, 0, 0);
+            startDate.setDate(startDate.getDate() + 1);
+            startDate.setUTCHours(0, 0, 0, 0);
 
-        mappedRound.votingStartDate = startDate;
+            mappedRound.votingStartDate = startDate;
+        }
 
         return mappedRound;
     }
