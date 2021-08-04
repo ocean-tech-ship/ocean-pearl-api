a51587752279a0dbd1d045c8e7d2a5c08917c7b2
Dominik Nolte <dominik.nolte@gemedo.de>
2021-08-04 20:16:38 +0200

    fix(global): give an heroku a port option

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     SwaggerModule.setup('api', app, document);
 
     app.enableCors();
-    await app.listen(3001);
+    await app.listen(process.env.PORT || 3001);
     console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
