a69da59a2753e4963e5f794dc17f8dad79f32c61
Dominik Nolte <dominik.nolte@gemedo.de>
2021-08-05 22:53:30 +0200

    refactor(pages): remove unnecessary function calls

--- src/pages/pages.controller.ts
+++ src/pages/pages.controller.ts
@@ -4,46 +4,34 @@ import { DaoProposal } from '../database/schemas/dao-proposal.schema';
 import { Project } from '../database/schemas/project.schema';
 import { Metrics } from '../metrics/interfaces/metrics.interface';
 import { CalculateMetricsService } from '../metrics/services/calculate-metrics.service';
-import { DaoFeaturedProjectsService } from '../projects/services/dao-featured-projects.service';
-import { FeaturedProjectsService } from '../projects/services/featured-projects.service';
 import { LatestProjectsService } from '../projects/services/latest-projects.service';
 
 @Controller('pages')
 export class PagesController {
-
     public constructor(
-        private getFeaturedProjectsService: FeaturedProjectsService,
         private getLatestProjectsService: LatestProjectsService,
-        private getDaoFeaturedProjectsService: DaoFeaturedProjectsService,
         private getLatestDaoProposalsService: GetLatestDaoProposalsService,
         private calculateMetricsService: CalculateMetricsService,
     ) {}
 
     @Get('index')
     async getIndexInfo(): Promise<{
-        featuredProjects: Project[],
-        latestProjects: Project[],
-        daoFeaturedProjects: Project[],
-        daoProposals: DaoProposal[],
-        metrics: Metrics
+        latestProjects: Project[];
+        daoProposals: DaoProposal[];
+        metrics: Metrics;
     }> {
         try {
-            const featuredProjects = await this.getFeaturedProjectsService.execute();
             const latestProjects = await this.getLatestProjectsService.execute();
-            const daoFeaturedProjects = await this.getDaoFeaturedProjectsService.execute();
             const daoProposals = await this.getLatestDaoProposalsService.execute();
             const metrics = await this.calculateMetricsService.execute();
 
             return {
-                featuredProjects: featuredProjects,
                 latestProjects: latestProjects,
-                daoFeaturedProjects: daoFeaturedProjects,
                 daoProposals: daoProposals,
-                metrics: metrics
+                metrics: metrics,
             };
         } catch (error: any) {
             throw error;
         }
     }
-
 }
