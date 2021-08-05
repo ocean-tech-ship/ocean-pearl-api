6af9fbdc33eb124108120e83b21183bda4c7e6a7
Dominik Nolte <dominik.nolte@gemedo.de>
2021-08-05 22:51:43 +0200

    feat(metrics): create a RoundMetricsInterface to show dates for the current and next round

--- src/metrics/interfaces/metrics.interface.ts
+++ src/metrics/interfaces/metrics.interface.ts
@@ -3,12 +3,16 @@ import { PaymentOptionEnum } from '../../database/enums/payment-option.enum';
 export interface Metrics {
     fundingRound: number;
     totalDaoProposals: number;
-    startDate?: Date;
-    submissionEndDate: Date;
-    votingStartDate: Date;
-    endDate: Date;
-    nextRoundStartDate?: Date;
+    currentRound: RoundMetrics;
+    nextRound: RoundMetrics;
     totalRequestedFunding: number;
     totalVotes: number;
     paymentOption: PaymentOptionEnum;
 }
+
+export interface RoundMetrics {
+    startDate: Date;
+    submissionEndDate: Date;
+    votingStartDate: Date;
+    endDate: Date;
+} 
