e09835cbf48943d006a0a42765182a98aa2979de
Dominik Nolte <dominik.nolte@gemedo.de>
2021-08-10 01:14:28 +0200

    feat(metrics): use the new service and the new findOne function to get the current and next round

--- src/metrics/services/calculate-metrics.service.ts
+++ src/metrics/services/calculate-metrics.service.ts
@@ -3,33 +3,25 @@ import { GetDaoProposalsByRoundService } from '../../dao-proposals/services/get-
 import { PaymentOptionEnum } from '../../database/enums/payment-option.enum';
 import { RoundRepository } from '../../database/repositories/round.repository';
 import { Round } from '../../database/schemas/round.schema';
+import { GetCurrentRoundService } from '../../rounds/services/get-current-round.service';
 import { Metrics, RoundMetrics } from '../interfaces/metrics.interface';
 
 @Injectable()
 export class CalculateMetricsService {
     constructor(
         private getDaoProposalsByRoundService: GetDaoProposalsByRoundService,
+        private getCurrentRoundService: GetCurrentRoundService,
         private roundRepository: RoundRepository,
     ) {}
 
     public async execute(): Promise<Metrics> {
         let totalVotesCount: number = 0;
         let totalRequestedFunding: number = 0;
-        const currentDate = new Date();
 
-        const currentRound = (
-            await this.roundRepository.getAll({
-                find: {
-                    startDate: { $lte: currentDate },
-                    votingEndDate: { $gte: currentDate },
-                },
-            })
-        )[0];
-        const nextRound = (
-            await this.roundRepository.getAll({
-                find: { round: currentRound.round + 1 },
-            })
-        )[0];
+        const currentRound = await this.getCurrentRoundService.execute();
+        const nextRound = await this.roundRepository.findOne({
+            find: { round: currentRound.round + 1 },
+        });
         const daoProposals = await this.getDaoProposalsByRoundService.execute(
             currentRound._id,
         );
@@ -55,14 +47,12 @@ export class CalculateMetricsService {
 
     private mapRoundMetrics(round: Round): RoundMetrics {
         return {
-            startDate: round?.startDate
-                ? new Date(round.startDate)
-                : null,
-            votingStartDate: round?.votingStartDate 
+            startDate: round?.startDate ? new Date(round.startDate) : null,
+            votingStartDate: round?.votingStartDate
                 ? new Date(round.votingStartDate)
                 : null,
             submissionEndDate: new Date(round.submissionEndDate),
             endDate: new Date(round.votingEndDate),
-        }
+        };
     }
 }
