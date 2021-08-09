a3b4a0e27dadfd0061cfe3debe6bb8fd3e627058
Dominik Nolte <dominik.nolte@gemedo.de>
2021-08-10 00:55:49 +0200

    feat(src): add the validationPipe in the global context

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -13,6 +14,7 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, document);
 
+    app.useGlobalPipes(new ValidationPipe());
     app.enableCors();
     await app.listen(process.env.PORT || 3001);
     console.log(`Application is running on: ${await app.getUrl()}`);
