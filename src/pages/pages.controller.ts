1194a02feb4eb235ed0e35d72ae8ce165ba217b6
Dominik Nolte <dominik.nolte@gemedo.de>
2021-08-10 01:15:51 +0200

    feat(pages): add api annotations

--- src/pages/pages.controller.ts
+++ src/pages/pages.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { GetLatestDaoProposalsService } from '../dao-proposals/services/get-latest-dao-proposals.service';
 import { DaoProposal } from '../database/schemas/dao-proposal.schema';
 import { Project } from '../database/schemas/project.schema';
@@ -6,6 +7,7 @@ import { Metrics } from '../metrics/interfaces/metrics.interface';
 import { CalculateMetricsService } from '../metrics/services/calculate-metrics.service';
 import { LatestProjectsService } from '../projects/services/latest-projects.service';
 
+@ApiTags('pages')
 @Controller('pages')
 export class PagesController {
     public constructor(
