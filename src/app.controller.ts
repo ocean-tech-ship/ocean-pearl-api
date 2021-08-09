2bc56d5b98575763155c5abebd6bcfd5cb90202b
Dominik Nolte <dominik.nolte@gemedo.de>
2021-08-10 00:52:47 +0200

    feat(src): add a redirect to the swagger docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
+
+    @Get()
+    @ApiResponse({ status: 301, description: 'Redirect to OpenApi docs'})
+    @Redirect('/api', 301)
+    public showSwaggerDocs(): void {}
 }
