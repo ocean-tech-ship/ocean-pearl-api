340bc6dbaa2868eb1d7bce01b14da90d61070cfe
Dominik Nolte <dominik.nolte@gemedo.de>
2021-08-10 01:13:40 +0200

    feat(metrics): add the getCurrentRoubdService to the providers

--- src/metrics/metrics.module.ts
+++ src/metrics/metrics.module.ts
@@ -4,11 +4,12 @@ import { DatabaseModule } from '../database/database.module';
 import { ProjectsModule } from '../projects/projects.module';
 import { MetricsController } from './metrics.controller';
 import { CalculateMetricsService } from './services/calculate-metrics.service';
+import { GetCurrentRoundService } from '../rounds/services/get-current-round.service';
 
 @Module({
     imports: [ProjectsModule, DaoProposalsModule, DatabaseModule],
     controllers: [MetricsController],
-    providers: [CalculateMetricsService],
+    providers: [CalculateMetricsService, GetCurrentRoundService],
     exports: [CalculateMetricsService],
 })
 export class MetricsModule {}
