72618f94a684e6ed4746a38245e9cbe21eb98b66
Marcel Haßlinger <me@marhali.de>
2021-08-21 15:08:02 +0200

    feat(deps): install cookie-parser

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
     SwaggerModule.setup('api', app, document);
 
     app.enableCors();
+    app.use(cookieParser());
+
     await app.listen(process.env.PORT || 3001);
     console.log(`Application is running on: ${await app.getUrl()}`);
 }
