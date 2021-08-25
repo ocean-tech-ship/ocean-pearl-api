8396c6120294c996fb8ccaabd43c72eba3a9ba3a
Marcel Haßlinger <me@marhali.de>
2021-08-25 11:13:24 +0200

    feat(controller): rework authentication controller to support access and refresh tokens

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,23 +3,34 @@ import {
     Controller,
     HttpStatus,
     Post,
+    Req,
     Res,
     UnauthorizedException,
+    UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { LoginRequest } from './auth.interface';
-import { Response } from 'express';
+import {
+    AuthenticatedUser,
+    LoginRequest,
+    RefreshJwtPayload,
+} from './auth.interface';
+import { Request, Response } from 'express';
 import { VerifyLoginService } from './verify-login/verify-login.service';
+import JwtRefreshGuard from './jwt-refresh.guard';
+import { SessionRepository } from '../database/repositories/session.repository';
+import { hash } from 'bcrypt';
+import { Session } from '../database/schemas/session.schema';
 
 @Controller('auth')
 export class AuthController {
     constructor(
         private readonly authService: AuthService,
         private readonly verifyLoginService: VerifyLoginService,
+        private readonly sessionRepository: SessionRepository,
     ) {}
 
     @Post('login')
-    login(@Body() request: LoginRequest, @Res() res: Response) {
+    async login(@Body() request: LoginRequest, @Res() res: Response) {
         if (!this.verifyLoginService.verifySignature(request)) {
             this.authService.clearJwtCookies(res);
             throw new UnauthorizedException(
@@ -36,13 +47,55 @@ export class AuthController {
             );
         }
 
-        this.authService.applyJwtCookies({ wallet: request.wallet }, res);
+        const user: AuthenticatedUser = { wallet: request.wallet };
+
+        this.authService.createAccessToken(user, res);
+        const refreshToken = this.authService.createRefreshToken(user, res);
+
+        await this.sessionRepository.create(<Session>{
+            createdAt: refreshToken.payload.createdAt,
+            walletAddress: refreshToken.payload.wallet,
+            hashedToken: await hash(refreshToken.jwt, 10),
+        });
+
         res.status(HttpStatus.CREATED).send();
     }
 
+    @UseGuards(JwtRefreshGuard)
+    @Post('refresh')
+    async refresh(@Req() req: Request, @Res() res: Response) {
+        const user = req.user as RefreshJwtPayload;
+
+        // Delete previous refresh token
+        await this.sessionRepository.deleteByWalletAddressAndCreatedAt(
+            user.wallet,
+            user.createdAt,
+        );
+
+        this.authService.createAccessToken(user, res);
+        const refreshToken = this.authService.createRefreshToken(user, res);
+
+        await this.sessionRepository.create(<Session>{
+            createdAt: refreshToken.payload.createdAt,
+            walletAddress: refreshToken.payload.wallet,
+            hashedToken: await hash(refreshToken.jwt, 10),
+        });
+
+        res.status(HttpStatus.OK).send();
+    }
+
     @Post('logout')
-    logout(@Res() res: Response) {
+    @UseGuards(JwtRefreshGuard)
+    async logout(@Req() req: Request, @Res() res: Response) {
+        const user = req.user as RefreshJwtPayload;
+
+        await this.sessionRepository.deleteByWalletAddressAndCreatedAt(
+            user.wallet,
+            user.createdAt,
+        );
+
         this.authService.clearJwtCookies(res);
+
         res.status(HttpStatus.OK).send();
     }
 }
