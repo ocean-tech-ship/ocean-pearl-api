ac0554a372f7a33fd750091dc8c537673c75d0f0
Marcel Haßlinger <me@marhali.de>
2021-08-28 15:33:03 +0200

    fix(env): also search for '.env' for production environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,6 +24,7 @@ if (process.env.NODE_ENV === 'production') {
     imports: [
         ConfigModule.forRoot({
             envFilePath: [
+                '.env',
                 `./${process.env.NODE_ENV}.env`,
                 `./enviroment/${process.env.NODE_ENV}.env`,
             ],
