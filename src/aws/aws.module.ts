3c1383965a626983cd30c1da35d503c5cbcb9a88
Dominik Nolte <dominik.nolte@gemedo.de>
2021-08-29 12:47:07 +0200

    fix(aws): add the s3 service to the module imports

--- src/aws/aws.module.ts
+++ src/aws/aws.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { S3ImageManagementService } from './s3/services/s3-image-management.service';
 
 @Module({
+    imports: [S3ImageManagementService],
     exports: [S3ImageManagementService],
 })
 export class AwsModule {}
