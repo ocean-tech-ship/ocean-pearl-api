f398718963e76b832ee7105fb671baf234724738
Marcel Haßlinger <me@marhali.de>
2021-08-29 14:37:45 +0200

    fix(auth): use correct cookie path for the refresh token

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -76,7 +76,7 @@ export class AuthService {
         res?.cookie(
             AuthService.SESSION_NAME_REFRESH,
             jwt,
-            this.createCookieOptions(Number(lifetime), '/account/refresh'),
+            this.createCookieOptions(Number(lifetime), '/management'),
         );
 
         return { payload, jwt };
@@ -89,7 +89,7 @@ export class AuthService {
         );
         res.clearCookie(
             AuthService.SESSION_NAME_REFRESH,
-            this.createCookieOptions(-1, '/account/refresh'),
+            this.createCookieOptions(-1, '/management'),
         );
     }
 
