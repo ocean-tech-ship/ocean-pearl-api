e41918e495c2a1e27f51fac7257601e5451ebd52
Dominik Nolte <dominik.nolte@gemedo.de>
2021-09-02 20:49:06 +0200

    feat(account): create a service that return projects with access for the wallet

---
+++ src/account/services/get-associated-projects.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { ProjectRepository } from '../../database/repositories/project.repository';
+import { Project } from '../../database/schemas/project.schema';
+import { ManagedProjectMapper } from '../mapper/managed-project.builder';
+import { AssociatedProject } from '../models/associated-project.model';
+
+@Injectable()
+export class GetAssociatedProjectsService {
+    public constructor(
+        private projectRepository: ProjectRepository,
+        private managedProjectMapper: ManagedProjectMapper,
+    ) {}
+
+    public async execute(walletAddress: string): Promise<AssociatedProject[]> {
+        const associatedProjects = await this.projectRepository.getAll({
+            find: { accessAddresses: walletAddress },
+        });
+
+        const mappedProjects = associatedProjects.map((project: Project) => {
+            return this.managedProjectMapper.map(project);
+        });
+
+        return mappedProjects;
+    }
+}
