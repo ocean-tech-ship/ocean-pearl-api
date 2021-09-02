78f4578d4bd68bbccb8c4ad3770b260e92790cea
Dominik Nolte <dominik.nolte@gemedo.de>
2021-09-02 20:51:22 +0200

    feat(account): add services to the controller and creeate endpoints

--- src/account/controllers/account.controller.ts
+++ src/account/controllers/account.controller.ts
@@ -1,20 +1,77 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Put, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import {
+    ApiBody,
+    ApiOkResponse,
+    ApiTags,
+    ApiUnauthorizedResponse,
+    getSchemaPath
+} from '@nestjs/swagger';
 import { Request } from 'express';
-import { ApiTags } from '@nestjs/swagger';
 import { AuthenticatedUser } from '../../auth/models/authenticated-user.model';
+import { ProjectGuard } from '../guards/project.guard';
+import { AssociatedProject } from '../models/associated-project.model';
+import { UpdatedProject } from '../models/updated-project.model';
+import { GetAssociatedProjectsService } from '../services/get-associated-projects.service';
+import { UpdateProjectService } from '../services/update-project.servce';
 
 @ApiTags('account')
+@UseGuards(AuthGuard('jwt'))
 @Controller('account')
 export class AccountController {
+    public constructor(
+        private getAssociatedProjectsService: GetAssociatedProjectsService,
+        private updateProjectService: UpdateProjectService
+    ) {}
+
     @Get()
-    @UseGuards(AuthGuard())
-    getProjects(@Req() req: Request) {
-        const user = req.user as AuthenticatedUser;
-        // TODO: fill with attached projects
+    @ApiUnauthorizedResponse({ description: 'Unauthorized.' })
+    @ApiOkResponse({
+        description: 'List of all associated projects',
+        schema: {
+            properties: {
+                projects: {
+                    items: { $ref: getSchemaPath(AssociatedProject) },
+                    type: 'array',
+                },
+                wallet: {
+                    type: 'string',
+                },
+            },
+        },
+    })
+    public async getProjects(
+        @Req() request: Request,
+    ): Promise<{
+        wallet: string;
+        projects: AssociatedProject[];
+    }> {
+        const user = request.user as AuthenticatedUser;
+
+        const projects = await this.getAssociatedProjectsService.execute(
+            user.wallet,
+        );
+
         return {
             wallet: user.wallet,
-            projects: [],
+            projects: projects,
         };
+    }
+
+    @Put('/project')
+    @ApiBody({
+        type: UpdatedProject,
+    })
+    @ApiOkResponse({ description: 'Ok.' })
+    @ApiUnauthorizedResponse({ description: 'Unauthorized.' })
+    @UseGuards(ProjectGuard)
+    public async updateProject(
+        @Body('project') project: UpdatedProject,
+    ): Promise<void> {
+        try {
+            this.updateProjectService.execute(project);
+        } catch (error) {
+            throw error;
+        }
     }
 }
