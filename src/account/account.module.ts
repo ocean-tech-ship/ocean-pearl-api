8f1de36e90e80562f4c31d40aa51d159dafd2511
Dominik Nolte <dominik.nolte@gemedo.de>
2021-09-02 20:50:47 +0200

    feat(account): add all classes to the account module

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AccountController } from './controllers/account.controller';
 import { AuthModule } from '../auth/auth.module';
-import { AuthController } from './controllers/auth.controller';
+import { AwsModule } from '../aws/aws.module';
+import { S3ImageManagementService } from '../aws/s3/services/s3-image-management.service';
 import { DatabaseModule } from '../database/database.module';
+import { AccountController } from './controllers/account.controller';
+import { AuthController } from './controllers/auth.controller';
+import { ManagedProjectMapper } from './mapper/managed-project.builder';
+import { GetAssociatedProjectsService } from './services/get-associated-projects.service';
+import { UpdateProjectService } from './services/update-project.servce';
 
 @Module({
-    imports: [AuthModule, DatabaseModule],
+    imports: [AuthModule, DatabaseModule, AwsModule],
     controllers: [AccountController, AuthController],
+    providers: [
+        GetAssociatedProjectsService,
+        ManagedProjectMapper,
+        UpdateProjectService,
+        S3ImageManagementService,
+    ],
 })
 export class AccountModule {}
