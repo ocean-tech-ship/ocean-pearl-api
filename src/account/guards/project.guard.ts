d961771cebadab06d9066f8894beec7ebd933c32
Dominik Nolte <dominik.nolte@gemedo.de>
2021-09-06 19:44:20 +0200

    refactor(account): use the param for the id and filter the addresses

--- src/account/guards/project.guard.ts
+++ src/account/guards/project.guard.ts
@@ -1,4 +1,9 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+    CanActivate,
+    ExecutionContext,
+    Injectable,
+    UnauthorizedException,
+} from '@nestjs/common';
 import { ProjectRepository } from '../../database/repositories/project.repository';
 
 @Injectable()
@@ -8,28 +13,18 @@ export class ProjectGuard implements CanActivate {
     public async canActivate(context: ExecutionContext): Promise<boolean> {
         const request = context.switchToHttp().getRequest();
         const user = request.user;
-        const projectId = request.body.id;
-
-        console.log(user, request.body);
-        console.log(request);
+        const projectId = request.params?.id;
 
         if (!user || !projectId) {
             throw new UnauthorizedException();
         }
 
-        const projects = await this.projectRepository.getAll({
-            find: { accessWallets: user.wallet },
+        const project = await this.projectRepository.findOne({
+            find: { id: projectId },
         });
 
-        console.log(projects)
-
-        for (const project of projects) {
-            if (
-                project.id === projectId &&
-                project.accessAddresses.includes(user.wallet)
-            ) {
-                return true;
-            }
+        if (project?.accessAddresses?.includes(user.wallet.toLowerCase())) {
+            return true;
         }
 
         throw new UnauthorizedException();
