c31efc9a2c0f532c2fbff758f7a2f21ae4b48554
Dominik Nolte <dominik.nolte@gemedo.de>
2021-09-06 19:37:32 +0200

    refactor(aws): use mimetypes map and use the config service to create the s3Client

--- src/aws/aws.module.ts
+++ src/aws/aws.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { S3ImageManagementService } from './s3/services/s3-image-management.service';
 
 @Module({
-    imports: [S3ImageManagementService],
+    imports: [ConfigModule],
+    providers: [S3ImageManagementService],
     exports: [S3ImageManagementService],
 })
 export class AwsModule {}
--- src/aws/tests/integration/services/s3-image-management.service.spec.ts
+++ src/aws/tests/integration/services/s3-image-management.service.spec.ts
@@ -1,3 +1,4 @@
+import { ConfigModule } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { S3ImageManagementService } from '../../../s3/services/s3-image-management.service';
 
@@ -6,10 +7,13 @@ describe('S3ImageManagementService', () => {
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            imports: [S3ImageManagementService],
+            imports: [ConfigModule],
+            providers: [S3ImageManagementService],
         }).compile();
 
-        service = module.get<S3ImageManagementService>(S3ImageManagementService);
+        service = module.get<S3ImageManagementService>(
+            S3ImageManagementService,
+        );
     });
 
     it('should be defined', () => {
