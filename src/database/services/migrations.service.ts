5edbb0b86fefe4bcb6dcd7c5df187a34cae6fc26
Dominik Nolte <dominik.nolte@gemedo.de>
2021-09-24 19:47:11 +0200

    feat(database): create a service that will automatically run all migrations

---
+++ src/database/services/migrations.service.ts
@@ -0,0 +1,104 @@
+import { Logger, OnModuleInit } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { Connection } from 'mongoose';
+import { MigrationStatusEnum } from '../enums/migration-status.enum';
+import { MigrationInterface } from '../interfaces/migration.interface';
+import { Migration } from '../schemas/migration.schema';
+
+export class MigrationService implements OnModuleInit {
+    private readonly logger = new Logger(MigrationService.name);
+    private migrations: MigrationInterface[] = [];
+
+    public constructor(@InjectConnection() private connection: Connection) {}
+
+    public async onModuleInit(): Promise<void> {
+        await this.initMigrations();
+        await this.up();
+    }
+
+    public async up(): Promise<void> {
+        this.logger.log('Starting Migrations Up.');
+        const migrationModel = this.connection.model<Migration>('Migration');
+
+        try {
+            for (const migration of this.migrations) {
+                if (
+                    await migrationModel.exists({
+                        version: migration.getVersion(),
+                        status: {
+                            $in: [
+                                MigrationStatusEnum.Revert,
+                                MigrationStatusEnum.Down,
+                                MigrationStatusEnum.Up,
+                            ],
+                        },
+                    })
+                ) {
+                    continue;
+                }
+
+                await migration.up(this.connection);
+                await migrationModel.findOneAndUpdate(
+                    { version: migration.getVersion() },
+                    {
+                        version: migration.getVersion(),
+                        status: MigrationStatusEnum.Up,
+                    } as Migration,
+                    { upsert: true },
+                );
+            }
+        } catch (error) {
+            throw error;
+        }
+
+        this.logger.log('Finished Migrations Up.');
+    }
+
+    @Cron(CronExpression.EVERY_HOUR, {
+        name: 'Migration revert',
+        timeZone: 'Europe/Berlin',
+    })
+    public async down(): Promise<void> {
+        this.logger.log('Starting Migrations Down.');
+        const migrationModel = this.connection.model('Migration');
+
+        try {
+            for (const migration of this.migrations) {
+                if (
+                    await !migrationModel.exists({
+                        version: migration.getVersion(),
+                        status: MigrationStatusEnum.Revert,
+                    })
+                ) {
+                    continue;
+                }
+
+                await migration.down(this.connection);
+                await migrationModel.updateOne({
+                    version: migration.getVersion(),
+                    status: MigrationStatusEnum.Down,
+                } as Migration);
+            }
+        } catch (error) {
+            throw error;
+        }
+
+        this.logger.log('Finished Migrations Down.');
+    }
+
+    private async initMigrations(): Promise<void> {
+        const path = require('path');
+        const fs = require('fs');
+        const directoryPath = path.join(__dirname, '../migrations/');
+
+        const files = fs
+            .readdirSync(directoryPath)
+            .filter((file: any) => file.endsWith('.js'));
+
+        for (const file of files) {
+            const migration = await import(directoryPath + file);
+            this.migrations.push(new migration.default());
+        }
+    }
+}
