434b51397628727531a8e66d17f4e1c8a300add6
Dominik Nolte <dominik.nolte@gemedo.de>
2021-10-09 22:03:35 +0200

    refactor(rounds): delete unused service

--- src/rounds/rounds.module.ts
+++ src/rounds/rounds.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
+import { LeaderboardController } from './controller/leaderboard.controller';
+import { LeaderboardProposalMapper } from './mapper/leaderboard-proposal.mapper';
+import { GenerateLeaderboardService } from './services/generate-leaderboard.service';
 import { GetCurrentRoundService } from './services/get-current-round.service';
-import { GetRoundsAmountService } from './services/get-rounds-amount.service';
 
 @Module({
+    controllers: [LeaderboardController],
     imports: [DatabaseModule],
-    providers: [GetCurrentRoundService, GetRoundsAmountService],
-    exports: [GetCurrentRoundService, GetRoundsAmountService],
+    providers: [
+        GetCurrentRoundService,
+        GenerateLeaderboardService,
+        LeaderboardProposalMapper,
+    ],
+    exports: [GetCurrentRoundService],
 })
 export class RoundsModule {}
