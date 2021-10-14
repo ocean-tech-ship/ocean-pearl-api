7819006042f57c419f3e910dc5502eb8ff174ff3
Dominik Nolte <dominik.nolte@gemedo.de>
2021-10-15 00:07:06 +0200

    feat(rounds): create basic test class for the controller

---
+++ src/rounds/tests/e2e/leaderboard.controller.spec.ts
@@ -0,0 +1,43 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../../../app.module';
+import { AwsModule } from '../../../aws/aws.module';
+import { DatabaseModule } from '../../../database/database.module';
+import { LeaderboardProposalBuilder } from '../../builder/leaderboard-proposal.builder';
+import { LeaderboardController } from '../../controller/leaderboard.controller';
+import { GenerateLeaderboardService } from '../../services/generate-leaderboard.service';
+import { GetCurrentRoundService } from '../../services/get-current-round.service';
+import { EarmarkedPropsoalStrategy } from '../../strategies/earmaked-proposal.strategy';
+import { GeneralPropsoalStrategy } from '../../strategies/general-proposal.strategy';
+import { StrategyCollection } from '../../strategies/strategy.collection';
+import { WontReceiveFundingStrategy } from '../../strategies/wont-receive-funding.strategy';
+
+describe('LeaderboardController', () => {
+    let module: TestingModule;
+    let controller: LeaderboardController;
+
+    beforeEach(async () => {
+        module = await Test.createTestingModule({
+            imports: [DatabaseModule, AppModule, AwsModule],
+            controllers: [LeaderboardController],
+            providers: [
+                GetCurrentRoundService,
+                GenerateLeaderboardService,
+                LeaderboardProposalBuilder,
+                StrategyCollection,
+                EarmarkedPropsoalStrategy,
+                GeneralPropsoalStrategy,
+                WontReceiveFundingStrategy,
+            ],
+        }).compile();
+
+        controller = module.get<LeaderboardController>(LeaderboardController);
+    });
+
+    afterAll(async () => {
+        await module.close();
+    });
+
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
+});
