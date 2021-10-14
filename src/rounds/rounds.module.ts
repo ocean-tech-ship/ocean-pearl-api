d4128b845a167eda478d460488c287049600c33b
Dominik Nolte <dominik.nolte@gemedo.de>
2021-10-15 00:06:28 +0200

    feat(rounds): add all new classes and services to the module

--- src/rounds/rounds.module.ts
+++ src/rounds/rounds.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { LeaderboardController } from './controller/leaderboard.controller';
-import { LeaderboardProposalMapper } from './mapper/leaderboard-proposal.mapper';
+import { LeaderboardProposalBuilder } from './builder/leaderboard-proposal.builder';
 import { GenerateLeaderboardService } from './services/generate-leaderboard.service';
 import { GetCurrentRoundService } from './services/get-current-round.service';
+import { EarmarkedPropsoalStrategy } from './strategies/earmaked-proposal.strategy';
+import { GeneralPropsoalStrategy } from './strategies/general-proposal.strategy';
+import { StrategyCollection } from './strategies/strategy.collection';
+import { WontReceiveFundingStrategy } from './strategies/wont-receive-funding.strategy';
 
 @Module({
     controllers: [LeaderboardController],
@@ -11,7 +15,11 @@ import { GetCurrentRoundService } from './services/get-current-round.service';
     providers: [
         GetCurrentRoundService,
         GenerateLeaderboardService,
-        LeaderboardProposalMapper,
+        LeaderboardProposalBuilder,
+        StrategyCollection,
+        EarmarkedPropsoalStrategy,
+        GeneralPropsoalStrategy,
+        WontReceiveFundingStrategy,
     ],
     exports: [GetCurrentRoundService],
 })
