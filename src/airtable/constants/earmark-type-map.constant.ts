6566b156d9387d80ef5bc495466cc2f1dc375757
Dominik Nolte <dominik.nolte@gemedo.de>
2021-11-15 00:07:23 +0100

    feat(airtable): create a map for earmarkTypes

---
+++ src/airtable/constants/earmark-type-map.constant.ts
@@ -0,0 +1,9 @@
+import { EarmarkTypeEnum } from '../../database/enums/earmark-type.enum';
+
+export const EarmarkTypeMap = {
+    'Outreach': EarmarkTypeEnum.Outreach,
+    'New Outreach': EarmarkTypeEnum.NewOutreach,
+    'New Entrants': EarmarkTypeEnum.NewEntrants,
+    'Core Tech': EarmarkTypeEnum.CoreTech,
+    'New General': EarmarkTypeEnum.NewGeneral,
+}
