1bad00ab279015c24c3cbd95bc2ee8921163a94d
Dominik Nolte <dominik.nolte@gemedo.de>
2021-12-10 22:55:29 +0100

    feat(database): define pagination and read options

--- src/database/interfaces/pagination-options.interface.ts
+++ src/database/interfaces/pagination-options.interface.ts
@@ -1,8 +1,28 @@
-import { FilterQuery } from 'mongoose';
+import { PopulateOptions, QueryOptions } from 'mongoose';
 
-export interface PaginationOptions {
-    page: number;
-    limit: number;
-    find?: FilterQuery<any>;
-    sort?: FilterQuery<any>;
+export interface PaginationOptions<T> {
+    select?: T | string | undefined;
+    sort?: Object | string | undefined;
+    populate?:
+        | PopulateOptions[]
+        | string[]
+        | PopulateOptions
+        | string
+        | PopulateOptions
+        | undefined;
+    projection?: any;
+    lean?: boolean | undefined;
+    leanWithId?: boolean | undefined;
+    page?: number | undefined;
+    limit?: number | undefined;
+    useEstimatedCount?: boolean | undefined;
+    forceCountFn?: boolean | undefined;
+    allowDiskUse?: boolean | undefined;
+    read?: ReadOptions | undefined;
+    options?: QueryOptions | undefined;
+}
+
+export interface ReadOptions {
+    pref: string;
+    tags?: any[] | undefined;
 }
