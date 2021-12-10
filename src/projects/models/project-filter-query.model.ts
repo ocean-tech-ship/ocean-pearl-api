d79f0c7bbc3f9bd03c145a4189c4eff62de7bafc
Dominik Nolte <dominik.nolte@gemedo.de>
2021-12-10 23:11:21 +0100

    feat(projects): add a page and a limit to the possible query values

--- src/projects/models/project-filter-query.model.ts
+++ src/projects/models/project-filter-query.model.ts
@@ -1,5 +1,6 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsEnum, IsOptional, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
 import { CategoryEnum } from '../../database/enums/category.enum';
 
 export class ProjectFilterQuery {
@@ -18,4 +19,24 @@ export class ProjectFilterQuery {
     @IsOptional()
     @IsString()
     search: string;
+
+    @ApiPropertyOptional({
+        description: 'The current page',
+        type: Number,
+        default: 0,
+    })
+    @IsOptional()
+    @Type(() => Number)
+    @IsNumber()
+    page: number;
+
+    @ApiPropertyOptional({
+        description: 'Amount of Proposals per page',
+        type: Number,
+        default: 0,
+    })
+    @IsOptional()
+    @Type(() => Number)
+    @IsNumber()
+    limit: number;
 }
