6118e680538ae3ceb44c1036ec5e0855f98348da
Dominik Nolte <dominik.nolte@gemedo.de>
2021-12-10 23:25:19 +0100

    feat(dao-proposals): add typehints and fix the round filter logic

--- src/dao-proposals/services/get-open-dao-proposals.service.ts
+++ src/dao-proposals/services/get-open-dao-proposals.service.ts
@@ -2,24 +2,32 @@ import { Injectable } from '@nestjs/common';
 import { DaoProposalStatusEnum } from '../../database/enums/dao-proposal-status.enum';
 import { FindQuery } from '../../database/interfaces/find-query.interface';
 import { DaoProposalRepository } from '../../database/repositories/dao-proposal.repository';
-import { DaoProposal } from '../../database/schemas/dao-proposal.schema';
+import { RoundRepository } from '../../database/repositories/round.repository';
+import {
+    DaoProposal,
+    DaoProposalType,
+} from '../../database/schemas/dao-proposal.schema';
+import { Round } from '../../database/schemas/round.schema';
 
 @Injectable()
-export class GetOpenDaoProposalsService
-{
+export class GetOpenDaoProposalsService {
     constructor(
-        private daoProposalRepository: DaoProposalRepository
+        private daoProposalRepository: DaoProposalRepository,
+        private roundRepository: RoundRepository        
     ) {}
 
-    public async execute(
-        fundingRound?: number
-    ): Promise<DaoProposal[]> {
-        let query: FindQuery = {
+    public async execute(fundingRound?: number): Promise<DaoProposal[]> {
+        let query: FindQuery<DaoProposalType> = {
             find: { status: DaoProposalStatusEnum.Running },
         };
 
         if (fundingRound) {
-            query.find.fundingRound = fundingRound;
+            const filteredRound: Round = await this.roundRepository.findOneRaw({
+                find: {
+                    round: fundingRound
+                }
+            }); 
+            query.find.fundingRound = filteredRound._id;
         }
 
         return await this.daoProposalRepository.getAll(query);
