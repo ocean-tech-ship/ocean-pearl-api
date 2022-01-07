41f17af3554e16ffe335fe4ce281c09cdbf0bf41
Dominik Nolte <dominik.nolte@gemedo.de>
2022-01-07 21:31:29 +0100

    Revert "fix(leaderboard): remove the caching"
    This reverts commit d37914d789ce515df86c5584cd804629fd1259f8.

--- src/rounds/services/generate-leaderboard.service.ts
+++ src/rounds/services/generate-leaderboard.service.ts
@@ -28,13 +28,11 @@ export class GenerateLeaderboardService {
 
     public async execute(): Promise<Leaderboard> {
         const round = await this.getCurrentRoundService.execute();
+        const cachedLeaderboard = await this.leaderboardCacheService.getFromCache(round.round);
 
-        // TODO: revert this later on
-        // const cachedLeaderboard = await this.leaderboardCacheService.getFromCache(round.round);
-
-        // if (cachedLeaderboard) {
-        //     return cachedLeaderboard;
-        // }
+        if (cachedLeaderboard) {
+            return cachedLeaderboard;
+        }
 
         let leaderboard: Leaderboard = this.leaderboardMapper.map(round);
         let leaderboardProposals: LeaderboardProposal[] = [];
