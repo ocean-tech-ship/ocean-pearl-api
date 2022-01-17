83945152666aca029b5a5777c1016dab38e56f40
Dominik Nolte <dominik.nolte@gemedo.de>
2022-01-17 01:05:07 +0100

    feat(rounds): reassign unsued earmark funding to the general grantpool

--- src/rounds/services/generate-leaderboard.service.ts
+++ src/rounds/services/generate-leaderboard.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { DaoProposalStatusEnum } from '../../database/enums/dao-proposal-status.enum';
+import { EarmarkTypeEnum } from '../../database/enums/earmark-type.enum';
 import { PaymentOptionEnum } from '../../database/enums/payment-option.enum';
 import { DaoProposalRepository } from '../../database/repositories/dao-proposal.repository';
 import { LeaderboardProposalBuilder } from '../builder/leaderboard-proposal.builder';
@@ -36,8 +37,6 @@ export class GenerateLeaderboardService {
 
         let leaderboard: Leaderboard = this.leaderboardMapper.map(round);
         let leaderboardProposals: LeaderboardProposal[] = [];
-        let lowestEarmarkVotes: number;
-        let lowestGeneralVotes: number;
 
         const proposals = await this.daoProposalRepository.getAll({
             find: { fundingRound: round._id },
@@ -58,40 +57,63 @@ export class GenerateLeaderboardService {
                     : proposal.requestedGrantToken;
             leaderboard.totalVotes += proposal.votes + proposal.counterVotes;
 
-            leaderboardProposals.push(
-                await this.leaderboardProposalBuilder.build(proposal, round),
-            );
+            leaderboardProposals.push(await this.leaderboardProposalBuilder.build(proposal, round));
             leaderboard.amountProposals++;
         }
 
         leaderboardProposals.sort(
-            (
-                current: LeaderboardProposal,
-                next: LeaderboardProposal,
-            ): number => {
+            (current: LeaderboardProposal, next: LeaderboardProposal): number => {
                 return next.effectiveVotes - current.effectiveVotes;
             },
         );
 
-        lowestEarmarkVotes = lowestGeneralVotes =
-            leaderboardProposals[0].effectiveVotes;
-
-        for (let proposal of leaderboardProposals) {
-            let strategy = this.strategyCollection.findMatchingStrategy(
-                proposal,
-                leaderboard,
-            );
-
-            ({ leaderboard, lowestEarmarkVotes, lowestGeneralVotes } =
-                strategy.execute(
-                    proposal,
-                    leaderboard,
-                    lowestEarmarkVotes,
-                    lowestGeneralVotes,
-                ));
-        }
+        leaderboard = this.assignFunding(leaderboard, leaderboardProposals);
 
         this.leaderboardCacheService.addToCache(leaderboard);
+        return leaderboard;
+    }
+
+    private assignFunding(leaderboard: Leaderboard, proposals: LeaderboardProposal[]): Leaderboard {
+        for (let proposal of proposals) {
+            let strategy = this.strategyCollection.findMatchingStrategy(proposal, leaderboard);
+
+            leaderboard = strategy.execute(proposal, leaderboard);
+        }
+
+        return this.assignUnusedFunding(leaderboard);
+    }
+
+    private assignUnusedFunding(leaderboard: Leaderboard): Leaderboard {
+        leaderboard.grantPools[EarmarkTypeEnum.General].potentialRemainingFunding =
+            leaderboard.grantPools[EarmarkTypeEnum.General].remainingFunding;
+
+        for (let [key, pool] of Object.entries(leaderboard.grantPools)) {
+            if (key !== EarmarkTypeEnum.General) {
+                leaderboard.grantPools[EarmarkTypeEnum.General].remainingFunding +=
+                    pool.remainingFunding;
+                pool.remainingFunding = 0;
+            }
+        }
+
+        if (leaderboard.grantPools[EarmarkTypeEnum.General].remainingFunding === 0) {
+            return leaderboard;
+        }
+
+        const proposals = [
+            ...leaderboard.partiallyFundedProposals,
+            ...leaderboard.notFundedProposals,
+        ];
+        leaderboard.partiallyFundedProposals = [];
+        leaderboard.notFundedProposals = [];
+
+        for (let proposal of proposals) {
+            proposal.neededVotes = undefined;
+
+            let strategy = this.strategyCollection.findMatchingStrategy(proposal, leaderboard);
+
+            leaderboard = strategy.execute(proposal, leaderboard);
+        }
+
         return leaderboard;
     }
 }
