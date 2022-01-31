cc0fb78343dd6329e230c9612eee30e3ebaae4d0
Dominik Nolte <dominik.nolte@gemedo.de>
2022-01-31 21:04:20 +0100

    feat(rounds): use the new service and query to generate legacy leaderboards

--- src/rounds/controller/leaderboard.controller.ts
+++ src/rounds/controller/leaderboard.controller.ts
@@ -1,23 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Round } from '../../database/schemas/round.schema';
+import { LeaderboardFilterQuery } from '../models/leaderboard-filter-query.model';
 import { Leaderboard } from '../models/leaderboard.model';
 import { GenerateLeaderboardService } from '../services/generate-leaderboard.service';
+import { GenerateLegacyLeaderboardService } from '../services/generate-legacy-leaderboard.service';
+import { GetCurrentRoundService } from '../services/get-current-round.service';
 
 @ApiTags('rounds')
 @Controller('leaderboard')
 export class LeaderboardController {
     constructor(
-        private generateLeaderboardService: GenerateLeaderboardService
+        private generateLeaderboardService: GenerateLeaderboardService,
+        private generateLegacyLeaderboardService: GenerateLegacyLeaderboardService,
+        private getCurrentRoundService: GetCurrentRoundService,
     ) {}
 
     @Get()
     @ApiOkResponse({
         type: Leaderboard,
-        description: 'Leaderboard with an ordered list of proposals'
+        description: 'Leaderboard with an ordered list of proposals',
     })
-    public async calculateLeaderboard(): Promise<Leaderboard> {
+    @UsePipes(new ValidationPipe({ transform: true }))
+    public async calculateLeaderboard(
+        @Query() query: LeaderboardFilterQuery,
+    ): Promise<Leaderboard> {
         try {
-            return await this.generateLeaderboardService.execute();
+            const currentRound: Round = await this.getCurrentRoundService.execute();
+
+            if (currentRound.round > query.round) {
+                return await this.generateLegacyLeaderboardService.execute(query.round);
+            }
+
+            return await this.generateLeaderboardService.execute(currentRound);
         } catch (error) {
             throw error;
         }
