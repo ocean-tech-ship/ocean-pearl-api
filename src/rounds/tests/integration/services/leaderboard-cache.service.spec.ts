bac126996cb4527e0ce8361d96a83cc0fdc03784
Dominik Nolte <dominik.nolte@gemedo.de>
2022-01-31 21:13:40 +0100

    feat(rounds): format code and instantiate objects correctly

--- src/rounds/tests/integration/services/leaderboard-cache.service.spec.ts
+++ src/rounds/tests/integration/services/leaderboard-cache.service.spec.ts
@@ -4,6 +4,7 @@ import { AppModule } from '../../../../app.module';
 import { EarmarkTypeEnum } from '../../../../database/enums/earmark-type.enum';
 import { PaymentOptionEnum } from '../../../../database/enums/payment-option.enum';
 import { RoundStatusEnum } from '../../../enums/round-status.enum';
+import { GrantPool } from '../../../models/grant-pool.model';
 import { Leaderboard } from '../../../models/leaderboard.model';
 import { RoundsModule } from '../../../rounds.module';
 import { LeaderboardCacheService } from '../../../services/leaderboard-cache.service';
@@ -13,10 +14,10 @@ describe('LeaderboardCacheService', () => {
     let service: LeaderboardCacheService;
     let leaderboard: Leaderboard;
 
-    let voteStartDate = new Date();
-    voteStartDate.setHours(voteStartDate.getHours() - 2);
-    let voteEndDate = new Date();
-    voteStartDate.setHours(voteStartDate.getHours() + 2);
+    let votingStartDate = new Date();
+    votingStartDate.setHours(votingStartDate.getHours() - 2);
+    let votingEndDate = new Date();
+    votingEndDate.setHours(votingStartDate.getHours() + 2);
 
     beforeAll(async () => {
         module = await Test.createTestingModule({
@@ -26,33 +27,30 @@ describe('LeaderboardCacheService', () => {
 
         service = module.get<LeaderboardCacheService>(LeaderboardCacheService);
 
-        leaderboard = {
-            fundedProposals: [],
-            partiallyFundedProposals: [],
-            notFundedProposals: [],
+        leaderboard = new Leaderboard({
             amountProposals: 6,
             overallFunding: 100000,
             overallRequestedFunding: 230000,
             round: 10,
             totalVotes: 805000,
             grantPools: {
-                [EarmarkTypeEnum.NewEntrants]: {
+                [EarmarkTypeEnum.NewEntrants]: new GrantPool({
                     type: EarmarkTypeEnum.NewEntrants,
                     totalFunding: 20000,
                     remainingFunding: 20000,
-                },
-                [EarmarkTypeEnum.General]: {
+                }),
+                [EarmarkTypeEnum.General]: new GrantPool({
                     type: EarmarkTypeEnum.NewEntrants,
                     totalFunding: 80000,
                     remainingFunding: 80000,
-                }
+                }),
             },
             paymentOption: PaymentOptionEnum.Usd,
             status: RoundStatusEnum.VotingInProgress,
-            votingStartDate: voteStartDate,
-            votingEndDate: voteEndDate,
+            votingStartDate: votingStartDate,
+            votingEndDate: votingEndDate,
             maxVotes: 200000,
-        } as Leaderboard;
+        });
     });
 
     afterAll(async () => {
@@ -66,16 +64,12 @@ describe('LeaderboardCacheService', () => {
     it('should add a leaderboard to the cache', () => {
         service.addToCache(leaderboard);
 
-        expect(service.getFromCache(leaderboard.round)).resolves.toEqual(
-            leaderboard,
-        );
+        expect(service.getFromCache(leaderboard.round)).resolves.toEqual(leaderboard);
     });
 
     it('should remove the leaderboard', () => {
         service.removeFromCache(leaderboard.round);
 
-        expect(
-            service.getFromCache(leaderboard.round),
-        ).resolves.toBeUndefined();
+        expect(service.getFromCache(leaderboard.round)).resolves.toBeUndefined();
     });
 });
