65c07a61bdf6d367f508b6b1f3c65a99164e719d
Dominik Nolte <dominik.nolte@gemedo.de>
2022-03-16 23:45:11 +0100

    feat(utils): add the aws module to the imports

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AwsModule } from '../aws/aws.module';
 import { DatabaseModule } from '../database/database.module';
 import { ImageCleanupService } from './services/image-cleanup.service';
 import { ImageOptimizationService } from './services/image-optimization.service';
 
 @Module({
-    imports: [DatabaseModule],
+    imports: [DatabaseModule, AwsModule],
     providers: [ImageOptimizationService, ImageCleanupService],
     exports: [ImageOptimizationService, ImageCleanupService],
 })
