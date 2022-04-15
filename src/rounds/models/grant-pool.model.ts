357313e8b22fbec10b0bff397d10d07264cc6e22
Dominik Nolte <dominik.nolte@gemedo.de>
2022-04-16 01:28:56 +0200

    feat(rounds): add relevant fields for the calculation

--- src/rounds/models/grant-pool.model.ts
+++ src/rounds/models/grant-pool.model.ts
@@ -1,13 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { EarmarkTypeEnum } from '../../database/enums/earmark-type.enum';
 
-export interface GrantPoolProperties {
-    type?: EarmarkTypeEnum;
-    totalFunding?: number;
-    remainingFunding?: number;
-    potentialRemainingFunding?: number;
-}
-
 export class GrantPool {
     @ApiProperty({
         type: EarmarkTypeEnum,
@@ -19,12 +12,21 @@ export class GrantPool {
     totalFunding: number;
 
     @ApiProperty()
+    totalEffectiveVotes: number = 0
+
+    @ApiProperty()
     remainingFunding: number;
 
     @ApiProperty()
     potentialRemainingFunding?: number;
 
-    constructor(attributes: GrantPoolProperties = {}) {
+    @ApiProperty()
+    relevantFunding: number = 0;
+
+    @ApiProperty()
+    relevantEffectiveVotes: number = 0;
+
+    constructor(attributes: Partial<GrantPool> = {}) {
         for (let key in attributes) {
             this[key] = attributes[key];
         }
