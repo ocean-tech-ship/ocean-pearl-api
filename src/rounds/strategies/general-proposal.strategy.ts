87a7102e8aa7cf598dbb15f8a4bed2d1c3aaf918
Dominik Nolte <dominik.nolte@gemedo.de>
2022-04-16 01:50:23 +0200

    feat(rounds): update the funding distribution for general proposals

--- src/rounds/strategies/general-proposal.strategy.ts
+++ src/rounds/strategies/general-proposal.strategy.ts
@@ -3,47 +3,44 @@ import { EarmarkTypeEnum } from '../../database/enums/earmark-type.enum';
 import { leaderboardStrategyInterface } from '../interfaces/leaderboard-strategy.interface';
 import { LeaderboardProposal } from '../models/leaderboard-proposal.model';
 import { Leaderboard } from '../models/leaderboard.model';
-import { CalculateNeededVotesService } from '../services/calculate-needed-votes.service';
 
 @Injectable()
 export class GeneralProposalStrategy implements leaderboardStrategyInterface {
-    constructor(private calculateNeededVotesService: CalculateNeededVotesService) {}
-
-    public canHandle(proposal: LeaderboardProposal, leaderboard: Leaderboard): boolean {
-        return (
-            leaderboard.grantPools[EarmarkTypeEnum.General].remainingFunding > 0 &&
-            proposal.effectiveVotes > 0 &&
-            proposal.yesVotes > proposal.noVotes
-        );
+    public canHandle(proposal: LeaderboardProposal): boolean {
+        return !proposal.isEarmarked;
     }
 
     public execute(proposal: LeaderboardProposal, leaderboard: Leaderboard): Leaderboard {
         leaderboard.maxVotes = Math.max(leaderboard.maxVotes, proposal.yesVotes, proposal.noVotes);
 
-        let remainingRequestedFunding: number =
-            proposal.requestedFunding - proposal.receivedFunding;
+        let remainingRequestedFunding: number = proposal.requestedFunding;
 
+        const generalFundingPerVote =
+            leaderboard.grantPools[EarmarkTypeEnum.General].relevantFunding /
+            leaderboard.grantPools[EarmarkTypeEnum.General].relevantEffectiveVotes;
         const receivingGeneralFunding: number = Math.min(
             remainingRequestedFunding,
-            leaderboard.grantPools[EarmarkTypeEnum.General].remainingFunding,
+            generalFundingPerVote * proposal.effectiveVotes,
         );
 
-        if (receivingGeneralFunding > 0) {
-            remainingRequestedFunding -= receivingGeneralFunding;
-            proposal.addToGrantPoolShare(EarmarkTypeEnum.General, receivingGeneralFunding);
+        proposal.addToGrantPoolShare(EarmarkTypeEnum.General, receivingGeneralFunding);
+        leaderboard.grantPools[EarmarkTypeEnum.General].remainingFunding = Math.max(
+            leaderboard.grantPools[EarmarkTypeEnum.General].remainingFunding -
+                receivingGeneralFunding,
+            0,
+        );
+        proposal.receivedFunding = receivingGeneralFunding;
 
-            leaderboard.grantPools[EarmarkTypeEnum.General].remainingFunding -=
-                receivingGeneralFunding;
-        }
-
-        proposal.receivedFunding = proposal.requestedFunding - remainingRequestedFunding;
-
-        if (remainingRequestedFunding === 0) {
+        if (proposal.requestedFunding === proposal.receivedFunding) {
             leaderboard.addToFundedProposals(proposal);
         } else {
-            proposal.neededVotes = this.calculateNeededVotesService.execute(proposal, leaderboard);
             leaderboard.addToPartiallyFundedProposals(proposal);
         }
+
+        leaderboard.grantPools[EarmarkTypeEnum.General].relevantFunding -=
+            proposal.grantPoolShare[EarmarkTypeEnum.General];
+        leaderboard.grantPools[EarmarkTypeEnum.General].relevantEffectiveVotes -=
+            proposal.effectiveVotes;
 
         return leaderboard;
     }
