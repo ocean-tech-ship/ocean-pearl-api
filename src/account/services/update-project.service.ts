a71dd383a9870fe385666caae1460b1d78e7271d
marhali <me@marhali.de>
2022-04-25 22:10:02 +0200

    feat(api): adjust update service to new workflow

--- src/account/services/update-project.service.ts
+++ src/account/services/update-project.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { S3ImageManagementService } from '../../aws/s3/services/s3-image-management.service';
 import { ImageRepository } from '../../database/repositories/image.repository';
 import { ProjectRepository } from '../../database/repositories/project.repository';
-import { Image } from '../../database/schemas/image.schema';
 import { UpdatedProject } from '../models/updated-project.model';
+import { Types } from 'mongoose';
 
 @Injectable()
 export class UpdateProjectService {
@@ -14,7 +14,7 @@ export class UpdateProjectService {
     ) {}
 
     public async execute(updatedProject: UpdatedProject): Promise<void> {
-        const dbProject = await this.projectRepository.findOne({
+        const dbProject = await this.projectRepository.findOneRaw({
             find: { id: updatedProject.id },
         });
 
@@ -28,45 +28,55 @@ export class UpdateProjectService {
         dbProject.socialMedia = updatedProject.socialMedia ?? dbProject.socialMedia;
 
         if (updatedProject.logo) {
+            // Delete old logo
             if (dbProject.logo) {
-                await this.s3ImageManagementService.deleteFileOnS3(dbProject.logo as Image);
-                delete dbProject.logo;
+                const oldLogo = await this.imageRepository.findOneRaw({
+                    find: { _id: dbProject.logo },
+                });
+
+                await this.s3ImageManagementService.deleteFileOnS3(oldLogo);
+                await this.imageRepository.delete({ find: { _id: oldLogo._id } });
+                dbProject.logo = undefined;
             }
 
-            // Only set new image if provided
+            // Set new logo - if present
             if (updatedProject.logo.id) {
                 // TODO: We need to verify that the image has not been used yet
-                dbProject.logo = await this.imageRepository.findOne({
+                const newLogo = await this.imageRepository.findOneRaw({
                     find: { id: updatedProject.logo.id },
                 });
+
+                dbProject.logo = newLogo._id;
             }
         }
 
         if (updatedProject.images) {
-            dbProject.images = dbProject.images ? (dbProject.images as Image[]) : [];
-            const deleteImages = [...dbProject.images];
+            dbProject.images = (dbProject.images as Types.ObjectId[]) ?? [];
+            const oldImageIds = [...dbProject.images];
 
             for (const image of updatedProject.images) {
-                const index = dbProject.images.findIndex((el) => el.id === image.id);
+                const imageModel = await this.imageRepository.findOneRaw({
+                    find: { id: image.id },
+                });
+                const index = oldImageIds.findIndex((id) => id.equals(imageModel._id));
 
                 if (index > -1) {
                     // Existing image and we want to keep it
-                    deleteImages.splice(index, 1);
+                    oldImageIds.splice(index, 1);
                 } else {
                     // New image
                     // TODO: We need to verify that the image has not been used yet
-                    const unassignedImage = await this.imageRepository.findOne({
-                        find: { id: image.id },
-                    });
-
-                    dbProject.images.push(unassignedImage);
+                    dbProject.images.push(imageModel._id);
                 }
             }
 
-            // Delete old images which are no longer present
-            for (const image of deleteImages) {
-                await this.s3ImageManagementService.deleteFileOnS3(image as Image);
-                const index = dbProject.images.findIndex((el) => el.id === image.id);
+            for (const id of oldImageIds) {
+                const imageModel = await this.imageRepository.findOneRaw({
+                    find: { _id: id },
+                });
+                await this.s3ImageManagementService.deleteFileOnS3(imageModel);
+                await this.imageRepository.delete({ find: { _id: id } });
+                const index = dbProject.images.findIndex((el) => el.equals(id));
                 dbProject.images.splice(index, 1);
             }
         }
