ddcf78b2f145b28d4725cd78d7f2dc8150f16187
Dominik Nolte <dominik.nolte@gemedo.de>
2022-05-23 00:36:00 +0200

    feat(utils): add new service to the providers and exports

--- src/utils/utils.module.ts
+++ src/utils/utils.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AwsModule } from '../aws/aws.module';
 import { DatabaseModule } from '../database/database.module';
+import { ImageAssociationService } from './services/image-association.service';
 import { ImageCleanupService } from './services/image-cleanup.service';
 import { ImageOptimizationService } from './services/image-optimization.service';
 
 @Module({
     imports: [DatabaseModule, AwsModule],
-    providers: [ImageOptimizationService, ImageCleanupService],
-    exports: [ImageOptimizationService, ImageCleanupService],
+    providers: [ImageOptimizationService, ImageCleanupService, ImageAssociationService],
+    exports: [ImageOptimizationService, ImageCleanupService, ImageAssociationService],
 })
 export class UtilsModule {}
