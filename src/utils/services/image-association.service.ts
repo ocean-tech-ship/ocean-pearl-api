262862e4729c459b164ed93f7b2c99d6b4a904d6
Dominik Nolte <dominik.nolte@gemedo.de>
2022-05-23 00:31:10 +0200

    feat(utils): add implementation for the association functions

--- src/utils/services/image-association.service.ts
+++ src/utils/services/image-association.service.ts
@@ -1,15 +1,39 @@
 import { Injectable } from '@nestjs/common';
-import { ImageRepository } from '../../database/repositories/image.repository';
+import { Types } from 'mongoose';
+import { DaoProposalRepository } from '../../database/repositories/dao-proposal.repository';
+import { ProjectRepository } from '../../database/repositories/project.repository';
+import { DaoProposal } from '../../database/schemas/dao-proposal.schema';
+import { Image } from '../../database/schemas/image.schema';
+import { Project } from '../../database/schemas/project.schema';
 
 @Injectable()
 export class ImageAssociationService {
-    public constructor(private imageRepository: ImageRepository) {}
+    public constructor(
+        private projectRepository: ProjectRepository,
+        private proposalRepository: DaoProposalRepository    
+    ) {}
 
-    public isProjectPictureOwner(): boolean {
-        return true;
+    public async isProjectPictureOwner(project: Project, image: Image): Promise<boolean> {
+        for (const projectImage of [...project.images, project.logo]) {
+            if (projectImage as Types.ObjectId === image._id) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    public isImageUnassociated(): boolean {
-        return true;
+    public async isImageUnassociated(image: Image): Promise<boolean> {
+        const associatedProject: Project = await this.projectRepository.findOneRaw({
+            find: {
+                $or: [{ logo: image._id }, { images: image._id }],
+            },
+        });
+
+        const associatedProposal: DaoProposal = await this.proposalRepository.findOneRaw({
+            find: { images: image._id },
+        });
+
+        return !associatedProject && !associatedProposal;
     }
 }
