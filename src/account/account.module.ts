3c8970c1d64ea994fcbcbdde68218436bf722731
Dominik Nolte <dominik.nolte@gemedo.de>
2022-07-29 00:18:08 +0200

    feat(account): import the new services and modules

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -8,19 +8,22 @@ import { ProjectGuard } from './guards/project.guard';
 import { ManagedProjectMapper } from './mapper/managed-project.mapper';
 import { GetAssociatedProjectsService } from './services/get-associated-projects.service';
 import { UpdateProjectService } from './services/update-project.service';
-import { UtilsModule } from '../utils/utils.module';
-import { ImageOptimizationService } from '../utils/services/image-optimization.service';
+import { ImageUtilsModule } from '../utils/image/image-utils.module';
 import { ImageUploadService } from './services/image-upload.service';
 import { ImageController } from './controllers/image.controller';
+import { CreateProjectService } from './services/create-project.service';
 import { ProjectCreationGuard } from './guards/project-creation.guard';
+import { ImageOptimizationService } from '../utils/image/services/image-optimization.service';
+import { WalletUtilsModule } from '../utils/wallet/wallet-utils.module';
 
 @Module({
-    imports: [AuthModule, DatabaseModule, AwsModule, UtilsModule],
+    imports: [AuthModule, DatabaseModule, AwsModule, ImageUtilsModule, WalletUtilsModule],
     controllers: [AccountController, AuthController, ImageController],
     providers: [
         GetAssociatedProjectsService,
         ManagedProjectMapper,
         UpdateProjectService,
+        CreateProjectService,
         ProjectGuard,
         ProjectCreationGuard,
         ImageOptimizationService,
