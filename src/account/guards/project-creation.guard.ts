4846180f44a685786dbe816e9bc5b34267d65d3a
Dominik Nolte <dominik.nolte@gemedo.de>
2022-07-29 00:00:18 +0200

    feat(account): create a guard for the project creation

--- src/account/guards/project-creation.guard.ts
+++ src/account/guards/project-creation.guard.ts
@@ -1,32 +1,42 @@
-import {
-    CanActivate,
-    ExecutionContext,
-    Injectable,
-    UnauthorizedException,
-    UseInterceptors,
-} from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-import * as multer from 'multer';
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { ProjectRepository } from '../../database/repositories/project.repository';
+import { WalletInfoService } from '../../utils/wallet/services/wallet-info.service';
 
 @Injectable()
 export class ProjectCreationGuard implements CanActivate {
-    public constructor(private projectRepository: ProjectRepository) {}
+    private readonly maxProjectAmount = 6;
+    private readonly minOceanAmount = 500;
+
+    public constructor(
+        private projectRepository: ProjectRepository,
+        private walletInfoService: WalletInfoService,
+    ) {}
 
     public async canActivate(context: ExecutionContext): Promise<boolean> {
-        const request = Object.assign(context.switchToHttp().getRequest<Request>());
-        const response = Object.assign(context.switchToHttp().getResponse<Response>());
-        await new Promise((resolve, reject) => {
-            multer().any()(request, response, function (err) {
-                if (err) reject(err);
+        const request = context.switchToHttp().getRequest();
+        const user = request.user;
 
-                console.log('GUARD!');
-                console.log(request);     
-                resolve(request);
-            });
+        const walletInfo = await this.walletInfoService.getCompleteInfo(user.wallet);
+        const createdProjects = await this.projectRepository.getAll({
+            find: {
+                'author.address': user.wallet.toLocaleLowerCase(),
+            },
         });
 
+        if (walletInfo.balance < this.minOceanAmount) {
+            throw new ForbiddenException(
+                `Users need at least ${this.minOceanAmount}OCEAN on the Mainnet, Polygon or BSC.`,
+            );
+        }
+
+        if (createdProjects.length > this.maxProjectAmount) {
+            throw new ForbiddenException(
+                `Users can only create up to ${this.maxProjectAmount} projects.`,
+            );
+        }
+
+        request.wallatInfo = walletInfo;
+
         return true;
-        // throw new UnauthorizedException();
     }
 }
