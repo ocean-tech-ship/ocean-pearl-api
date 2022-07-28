baa4ce8c43adce004fd8e54852fecc205730f8f8
Dominik Nolte <dominik.nolte@gemedo.de>
2022-07-29 00:19:32 +0200

    refactor(account): update the import paths

--- src/account/tests/integration/services/image-upload.service.spec.ts
+++ src/account/tests/integration/services/image-upload.service.spec.ts
@@ -2,7 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../../../../app.module';
 import { AwsModule } from '../../../../aws/aws.module';
 import { DatabaseModule } from '../../../../database/database.module';
-import { UtilsModule } from '../../../../utils/utils.module';
+import { ImageUtilsModule } from '../../../../utils/image/image-utils.module';
 import { ImageUploadService } from '../../../services/image-upload.service';
 
 describe('ImageUploadService', () => {
@@ -11,7 +11,7 @@ describe('ImageUploadService', () => {
 
     beforeAll(async () => {
         module = await Test.createTestingModule({
-            imports: [DatabaseModule, AppModule, AwsModule, UtilsModule],
+            imports: [DatabaseModule, AppModule, AwsModule, ImageUtilsModule],
             providers: [ImageUploadService],
         }).compile();
 
