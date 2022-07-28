0141c0e6d9789c30325201ce8fa5d056f47d8046
Dominik Nolte <dominik.nolte@gemedo.de>
2022-07-29 00:14:35 +0200

    feat(account): fix types, annotations and attribute names

--- src/account/models/create-project.model.ts
+++ src/account/models/create-project.model.ts
@@ -1,12 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsEnum, IsOptional, IsString, Length, MaxLength, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import {
+    ArrayMaxSize,
+    IsArray,
+    IsEnum,
+    IsObject,
+    IsOptional,
+    IsString,
+    Length,
+    MaxLength,
+    ValidateNested,
+} from 'class-validator';
 import { CategoryEnum } from '../../database/enums/category.enum';
+import { MediaHandlesEnum } from '../../database/enums/media-handles.enum';
+import { ImageUploadService } from '../services/image-upload.service';
+import { AssociatedImage } from './associated-project.model';
 import { CreateCryptoAddress } from './create-crypto-address.model';
-import { CreateTeamMember } from './create-team-member.model';
 
 export class CreateProject {
     @ApiProperty({
-        default: 'Add the project\'s name.',
+        default: "Add the project's name.",
     })
     @IsString()
     @Length(0, 64)
@@ -40,32 +53,60 @@ export class CreateProject {
     })
     @IsArray()
     @ValidateNested()
-    walletAddresses: CreateCryptoAddress[];
+    accessAddresses: CreateCryptoAddress[];
 
     @ApiProperty({
         type: CreateCryptoAddress,
-    })
-    @IsArray()
-    @ValidateNested()
-    paymentWalletAddresses: CreateCryptoAddress;
-
-    @ApiProperty({
-        type: () => new Map<string, string>(),
-        default: {
-            Oceanpearl: 'Oceanpearl.io',
-        },
-    })
-    @MaxLength(128, {
-        each: true,
-      })
-    @IsOptional()
-    links: Map<string, string>;
-
-    @ApiProperty({
-        type: CreateTeamMember,
         isArray: true,
     })
     @IsArray()
     @ValidateNested()
-    team: CreateTeamMember[];
+    paymentAddresses: CreateCryptoAddress[];
+
+    @ApiProperty({
+        type: Object,
+        additionalProperties: {
+            type: 'string',
+        },
+        default: {
+            [MediaHandlesEnum.Twitter]: 'Oceanpearl.io',
+        },
+    })
+    @Type(() => String)
+    @MaxLength(128, {
+        each: true,
+    })
+    @IsOptional()
+    mediaHandles: Map<MediaHandlesEnum, string>;
+
+    // @ApiProperty({
+    //     type: CreateTeamMember,
+    //     isArray: true,
+    // })
+    // @IsArray()
+    // @ValidateNested()
+    // team: CreateTeamMember[];
+
+    @ApiProperty()
+    @IsOptional()
+    @IsObject()
+    logo: AssociatedImage;
+
+    @ApiProperty({
+        type: AssociatedImage,
+        isArray: true,
+    })
+    @IsOptional()
+    @IsArray()
+    @ArrayMaxSize(ImageUploadService.IMAGE_MAX_AMOUNT, {
+        message: `Exceeded limit of ${ImageUploadService.IMAGE_MAX_AMOUNT} images`,
+    })
+    images: AssociatedImage[];
+
+    @ApiProperty({
+        default: 'Add a team name.',
+    })
+    @IsString()
+    @Length(0, 256)
+    teamName: string;
 }
