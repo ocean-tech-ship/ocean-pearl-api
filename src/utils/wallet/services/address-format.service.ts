d725a1ed3a5704c9cb26fd012b3316c9d1e603ee
marhali <me@marhali.de>
2022-08-13 18:45:48 +0200

    feat(wallet): enhance address format methods

--- src/utils/wallet/services/address-format.service.ts
+++ src/utils/wallet/services/address-format.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { getAddress } from 'ethers/lib/utils';
 
+export function formatAddresses(addresses: string[]): string[] {
+    return addresses.map((address) => formatAddress(address));
+}
+
+export function formatAddress(address?: string): string {
+    if (!address || address.length === 0) return address;
+    return getAddress(address);
+}
+
 @Injectable()
 export class AddressFormatService {
     public execute(address: string): string {
-        return getAddress(address);
+        return formatAddress(address);
     }
 }
