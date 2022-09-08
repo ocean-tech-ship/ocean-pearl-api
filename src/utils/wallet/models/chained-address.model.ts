ef55171b744731048a8d19f94a4a8eb5d2b96ed9
Dominik Nolte <dominik.nolte@gemedo.de>
2022-09-09 00:03:22 +0200

    feat(wallet): transform the address on setting action

--- src/utils/wallet/models/chained-address.model.ts
+++ src/utils/wallet/models/chained-address.model.ts
@@ -1,7 +1,10 @@
+import { Transform } from 'class-transformer';
 import { SupportedNetworksEnum } from '../../../database/enums/supported-networks.enum';
+import { formatAddress } from '../services/address-format.service';
 
 export class ChainedAddress {
     public network: SupportedNetworksEnum;
+    @Transform(({ value }) => formatAddress(value))
     public address: string;
 
     constructor(attributes: Partial<ChainedAddress>) {
