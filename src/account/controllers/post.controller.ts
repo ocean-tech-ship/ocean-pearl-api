beb93e52957c953ed28fb0d4a4ec84c92ef86c25
Dominik Nolte <dominik.nolte@gemedo.de>
2022-09-09 00:11:21 +0200

    feat(account): create a controller for the post routes

---
+++ src/account/controllers/post.controller.ts
@@ -0,0 +1,44 @@
+import {
+    Body,
+    Controller,
+    Injectable,
+    Post,
+    UseGuards,
+    UseInterceptors,
+    UsePipes,
+    ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBody, ApiOkResponse, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { WalletInfoParam } from '../../utils/wallet/decorators/wallet-info-parameter.decorator';
+import { WalletInfo } from '../../utils/wallet/models/wallet-info.model';
+import { WalletInfoInterceptor } from '../interceptor/wallet-info.interceptor';
+import { NewPost } from '../models/new-post.model';
+import { CreatePostService } from '../services/create-post.service';
+
+@ApiTags('posts')
+@UseGuards(AuthGuard('jwt-refresh'))
+@Controller('account')
+@Injectable()
+export class PostController {
+    public constructor(private createPostService: CreatePostService) {}
+
+    @Post('posts')
+    @ApiBody({
+        type: NewPost,
+    })
+    @ApiOkResponse({ description: 'Ok.' })
+    @ApiUnauthorizedResponse({ description: 'Unauthorized.' })
+    @UseInterceptors(WalletInfoInterceptor)
+    @UsePipes(new ValidationPipe({ transform: true }))
+    public async create(
+        @Body() post: NewPost,
+        @WalletInfoParam() walletInfo: WalletInfo,
+    ): Promise<void> {
+        try {
+            this.createPostService.execute(post, walletInfo);
+        } catch (error) {
+            throw error;
+        }
+    }
+}
