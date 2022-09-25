49c983c16b13e4416798756e0b6d9f6361e914bf
Dominik Nolte <dominik.nolte@gemedo.de>
2022-09-25 18:54:21 +0200

    feat(account): add the new service to the providers

--- src/account/account.module.ts
+++ src/account/account.module.ts
@@ -2,29 +2,31 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { AwsModule } from '../aws/aws.module';
 import { DatabaseModule } from '../database/database.module';
-import { AccountController } from './controllers/account.controller';
-import { AuthController } from './controllers/auth.controller';
-import { ProjectGuard } from './guards/project.guard';
-import { ManagedProjectMapper } from './mapper/managed-project.mapper';
-import { GetLinkedProjectsService } from './services/get-linked-projects.service';
-import { UpdateProjectService } from './services/update-project.service';
 import { ImageUtilsModule } from '../utils/image/image-utils.module';
-import { ImageUploadService } from './services/image-upload.service';
-import { ImageController } from './controllers/image.controller';
-import { CreateProjectService } from './services/create-project.service';
-import { ProjectCreationGuard } from './guards/project-creation.guard';
 import { ImageOptimizationService } from '../utils/image/services/image-optimization.service';
 import { WalletUtilsModule } from '../utils/wallet/wallet-utils.module';
-import { CreatePostService } from './services/create-post.service';
-import { WalletInfoInterceptor } from './interceptors/wallet-info.interceptor';
+import { AccountController } from './controllers/account.controller';
+import { AuthController } from './controllers/auth.controller';
+import { ImageController } from './controllers/image.controller';
 import { PostController } from './controllers/post.controller';
+import { PostGuard } from './guards/post.guard';
+import { ProjectCreationGuard } from './guards/project-creation.guard';
+import { ProjectGuard } from './guards/project.guard';
+import { WalletInfoInterceptor } from './interceptors/wallet-info.interceptor';
+import { LinkedProjectMapper } from './mapper/linked-project.mapper';
+import { CreatePostService } from './services/create-post.service';
+import { CreateProjectService } from './services/create-project.service';
+import { DeletePostService } from './services/delete-post.service';
+import { GetLinkedProjectsService } from './services/get-linked-projects.service';
+import { ImageUploadService } from './services/image-upload.service';
+import { UpdateProjectService } from './services/update-project.service';
 
 @Module({
     imports: [AuthModule, DatabaseModule, AwsModule, ImageUtilsModule, WalletUtilsModule],
     controllers: [AccountController, AuthController, ImageController, PostController],
     providers: [
         GetLinkedProjectsService,
-        ManagedProjectMapper,
+        LinkedProjectMapper,
         UpdateProjectService,
         CreateProjectService,
         ProjectGuard,
@@ -33,6 +35,8 @@ import { PostController } from './controllers/post.controller';
         ImageUploadService,
         CreatePostService,
         WalletInfoInterceptor,
+        DeletePostService,
+        PostGuard,
     ],
 })
 export class AccountModule {}
