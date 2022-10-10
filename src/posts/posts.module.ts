ce7433e7642b24e971d60d925b60cf5cd158879d
marhali <me@marhali.de>
2022-10-10 19:13:16 +0200

    feat(controller): add newest 3 posts to landing page endpoint

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,10 +3,12 @@ import { DatabaseModule } from '../database/database.module';
 import { PostsController } from './controllers/posts.controller';
 import { GetPostsService } from './services/get-posts.service';
 import { GetPostByIdService } from './services/get-post-by-id.service';
+import { LatestPostsService } from './services/latest-posts.service';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [PostsController],
-    providers: [GetPostsService, GetPostByIdService],
+    providers: [GetPostsService, GetPostByIdService, LatestPostsService],
+    exports: [LatestPostsService],
 })
 export class PostsModule {}
