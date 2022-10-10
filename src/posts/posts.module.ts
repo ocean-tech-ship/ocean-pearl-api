a9143f65ff36d5cb1296492e40c4013b09385808
marhali <me@marhali.de>
2022-10-10 19:02:53 +0200

    feat(controller): add endpoint for single post

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { PostsController } from './controllers/posts.controller';
 import { GetPostsService } from './services/get-posts.service';
+import { GetPostByIdService } from './services/get-post-by-id.service';
 
 @Module({
     imports: [DatabaseModule],
     controllers: [PostsController],
-    providers: [GetPostsService],
+    providers: [GetPostsService, GetPostByIdService],
 })
 export class PostsModule {}
